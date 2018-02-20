438e3a6dd30d77b06f2eb357bb8f82b76637f3d2
Marina Khismatullina <marina.k@uni-bonn.de>
2018-02-20 15:18:28 +0100

    Tidied up, all is working

--- Coding/functions.R
+++ Coding/functions.R
@@ -1,7 +1,6 @@
 # Epanechnikov kernel function,
 # which is defined as f(x) = 3/4(1-x^2)
 # for |x|<=1 and 0 elsewhere
-
 epanechnikov_kernel <- function(x)
 {
   if (abs(x)<=1)
@@ -13,21 +12,19 @@ epanechnikov_kernel <- function(x)
   return(result)
 }
 
-# Additive correction tern \lambda(h)
-# that depends only on the bandwidth h
 
+# Additive correction tern \lambda(h) that depends only on the bandwidth h
 lambda <- function(h)
 {
   result = tryCatch(sqrt(2*log(1/(2*h))), warning = function(w) print("h is exceeding h_max"))
-#  cat("lambda:", result)
   return(result)
 }
+
 
 # Kernel weight function that calculates
 # the vector of (\omega_{1,T}(u,h), \ldots, \omega_{T,T}(u,h)).
 # Meanwhile, the function also calculates
 # the norm ||K||_{u,h,T} of the kernel function.
-
 omega <- function(T, u, h, k_function)
 {
   result_temp = c()
@@ -44,19 +41,19 @@ omega <- function(T, u, h, k_function)
   return(result)
 }
 
+
 # Kernel average function \psi_T(u,h) that takes u, h, data
 # and the type of kernel function as arguments.
 # The data can be y_data for \hat{\Psi}_T or independent gaussian rv
 # z_temp = sigma*z for \Psi^star_T.
 # The output is one value for each u and h.
-
 psi_average <- function(data, u, h, k_function)
 {
   T = length(data)
   result = sum(omega(T, u, h, k_function)*data)
-#  cat("psihat_average:", result)
   return(result)
 }
+
 
 # Function that calculates the multiscale statistic \hat{\Psi}_T.
 # It takes the following entities as arguments.
@@ -65,6 +62,12 @@ psi_average <- function(data, u, h, k_function)
 #   k_function: type of kernel function
 #   sigmahat: the estimator of the square root of the long-run error variance \sigma^2
 # It produces the value of the test statistic as an output
+psihat_statistic <- function(y_data, g_t_set, kernel_function = epanechnikov_kernel, sigmahat) {
+  g_t_set$values <- abs(mapply(psi_average, u = g_t_set$u, h = g_t_set$h,
+                                    MoreArgs = list(data = y_data, k_function = kernel_function))/sigmahat) - .subset2(g_t_set, 'lambda')
+  result = max(g_t_set$values)
+  return(list(g_t_set, result))
+}
 
 #old version
 #psihat_statistic <- function(y_data, g_t_set, k_function = epanechnikov_kernel, sigmahat) {
@@ -76,24 +79,12 @@ psi_average <- function(data, u, h, k_function)
 #  return(result)
 #}
 
-psihat_statistic <- function(y_data, g_t_set, kernel_function = epanechnikov_kernel, sigmahat) {
-  g_t_set$values <- abs(mapply(psi_average, u = g_t_set$u, h = g_t_set$h,
-                                    MoreArgs = list(data = y_data, k_function = kernel_function))/sigmahat) - .subset2(g_t_set, 'lambda')
-  result = max(g_t_set$values)
-#  cat("Statistic:", result)
-  return(list(g_t_set, result))
-}
 
 # Function that calculates the auxiliary statistic \Psi^star_T.
 # The only difference with the previus function is in the return values.
-
-psistar_statistic <- function(z, g_t_set, k_function = epanechnikov_kernel, sigma) {
-  g_t_set_card = nrow(g_t_set)
-  z_temp = sigma*z
-  for (i in 1:g_t_set_card) {
-    g_t_set[['values']][i] <- abs(psi_average(z_temp, g_t_set[['u']][i], g_t_set[['h']][i], k_function)/sigma) - lambda(g_t_set[['h']][i])
-  }
+psistar_statistic <- function(y_data, g_t_set, kernel_function = epanechnikov_kernel, sigmahat) {
+  g_t_set$values <- abs(mapply(psi_average, u = g_t_set$u, h = g_t_set$h,
+                               MoreArgs = list(data = y_data, k_function = kernel_function))/sigmahat) - .subset2(g_t_set, 'lambda')
   result = max(g_t_set$values)
-#  cat("Statistic with a star:", result)
   return(result)
 }
--- Coding/main.R
+++ Coding/main.R
@@ -2,56 +2,106 @@ source("functions.R")
 library(microbenchmark)
 library(gsubfn)
 
-set.seed(1)
-sigma <- 2
-T<-100
-m <- numeric(T)
-for (i in 1:T) {
-  if (i/T < 0.5) {
-    m[i] <- 0
-  } else {
-    m[i] <- i - 0.5*T
-  }
-} 
-y_data <- m + rnorm(T, 0, sigma)
-sigmahat <- sigma
+#Defining constants 
+sigma <- 2 #square root of long run-variance
+T<-1000
 alpha <-0.05
 
-#Creating g_t_set like in Section 2.1
+
+#Creating g_t_set over which we are taking the maximum (from Section 2.1)
 u <- seq(4/T, 1, length.out = T/4)
 h <- seq(3/T, 1/4+3/T, length.out = T/20)
 
 g_t_set_temp <- expand.grid(u = u, h = h) #Creating a dataframe with all possible combination of u and h
 g_t_set_temp$values <-numeric(nrow(g_t_set_temp)) # Setting the values of the statistic to be zero
 
-#Subsetting u and h such that [u-h, u+h] lies in [0,1]
-g_t_set <- subset(g_t_set_temp, u - h >= 0 & u + h <= 1, select = c(u, h, values))
-g_t_set$lambda <- lambda(g_t_set[['h']])
+g_t_set <- subset(g_t_set_temp, u - h >= 0 & u + h <= 1, select = c(u, h, values)) #Subsetting u and h such that [u-h, u+h] lies in [0,1]
+g_t_set$lambda <- lambda(g_t_set[['h']]) #Calculating the lambda(h) in order to speed up the function psistar_statistic
 
-result <-psihat_statistic(y_data, g_t_set, epanechnikov_kernel, sigmahat)
-g_t_set_with_values <- result[[1]]
-psihat_statistic_value <- result[[2]]
 
-microbenchmark(psihat_statistic(y_data, g_t_set, epanechnikov_kernel, sigmahat))
+#If we have already calculated quantiles and stored them in a file 'distribution.RData'
+#then no need to calculate them once more, we just load them from this file.
+if(!file.exists('distribution.RData')) {
+  gaussian_statistic_distribution <- replicate(1000, {
+    z = rnorm(T, 0, 1)
+    z_temp = sigma * z
+    psistar_statistic(z_temp, g_t_set, epanechnikov_kernel, sigma)
+  })
+  save(gaussian_statistic_distribution, file = 'distribution.RData')
+  } else {
+  load('distribution.RData')
+  }
 
-gaussian_statistic_distribution <- replicate(1000, {
-  z = rnorm(T, 0, 1)
-  z_temp = sigma * z
-  psihat_statistic(z_temp, g_t_set, epanechnikov_kernel, sigma)[[2]]
-})
-save(gaussian_statistic_distribution, file = 'distribution.RData')
+#Calculate the quantiles for gaussian statistic defined in the previous step
 gaussian_quantile <- quantile(gaussian_statistic_distribution, probs = (1 - alpha), type = 1)
 
-if (psihat_statistic_value>=gaussian_quantile)
-{
-  cat("We reject H_0 with probability", alpha, "Psihat_statistic = ", psihat_statistic_value,
-      "Gaussian quantile value = ", gaussian_quantile)
-} else {
-  cat("We fail to reject H_0 with probability", alpha, "Psihat_statistic = ", psihat_statistic_value,
-      "Gaussian quantile value = ", gaussian_quantile)
-}
-g_t_set_rejected <- subset(g_t_set_with_values, values >= gaussian_quantile, select = c(u, h, values))
 
-number_rejected <- nrow(g_t_set_rejected)
-plot(NA, xlim=c(0,1), ylim=c(-5,100), xlab="x", ylab="y")
-segments(g_t_set_rejected[['u']] - g_t_set_rejected[['h']], g_t_set_rejected[['values']], g_t_set_rejected[['u']] + g_t_set_rejected[['h']], g_t_set_rejected[['values']])
+#Defining the data Y = m + noise
+set.seed(1) #For reproducibility
+m <- numeric(T)
+for (i in 1:T) {
+  if (i/T < 0.5) {m[i] <- 0} else {m[i] <- i - 0.5*T}
+} 
+y_data_1 <- m + rnorm(T, 0, sigma)#Adding to noise a function that is 0 on the first half and linear on the second half
+y_data_2 <- 1 + rnorm(T, 0, sigma)#Adding constant function m=1
+y_data_3 <- rnorm(T, 0, sigma)#Here the null hypothesis is true, m=0
+
+sigmahat <- sigma #Here will be the estimate of the square root of the long-run variance sigma^2
+
+
+#Auxiliary line for checking function time
+#microbenchmark(psihat_statistic(y_data, g_t_set, epanechnikov_kernel, sigmahat))
+
+
+#Function that takes the data as argument and plots the regions where
+#the corrected test statistic is bigger than the critical value of the 
+#gaussian version. This function needs to be called only in environment
+#with g_t_set and sigmahat defined.
+#Returns the set with regions and value of the statistic in this regions.
+plotting_all_rejected_intervals <-function(data, plotname){
+  
+  #Calculating our statistic
+  result <-psihat_statistic(data, g_t_set, epanechnikov_kernel, sigmahat)
+  g_t_set_with_values <- result[[1]]
+  psihat_statistic_value <- result[[2]]
+  
+  #And now the testing itself
+  if (psihat_statistic_value > gaussian_quantile) {
+    cat("We reject H_0 with probability", alpha, "Psihat_statistic = ", psihat_statistic_value,
+        "Gaussian quantile value = ", gaussian_quantile, "\n")
+
+    #The collection of intervals where the corrected test statistic lies above the critical value (as in (2.6))
+    a_t_set <- subset(g_t_set_with_values, values > gaussian_quantile, select = c(u, h, values))
+  
+    #The plotting itself
+    jpeg(filename=plotname)
+    ymaxlim = max(a_t_set$values)
+    yminlim = min(a_t_set$values)
+    plot(NA, xlim=c(0,1), ylim = c(yminlim, ymaxlim + 1), xlab="x", ylab="y")
+    segments(a_t_set[['u']] - a_t_set[['h']], a_t_set[['values']], a_t_set[['u']] + a_t_set[['h']], a_t_set[['values']])
+    dev.off()  
+
+    return(a_t_set)
+  } else {
+    cat("We fail to reject H_0 with probability", alpha, "Psihat_statistic = ", psihat_statistic_value,
+        "Gaussian quantile value = ", gaussian_quantile, "\n")
+    return(NULL)
+  }
+}
+
+a_t_set_1 <- plotting_all_rejected_intervals(y_data_1, "rightplot.jpg") #We expect to reject H_0 and the plot is mostly to the right
+a_t_set_2 <- plotting_all_rejected_intervals(y_data_2, "constantplot.jpg") #We expect to reject H_0 and the plot is everywhere
+a_t_set_3 <- plotting_all_rejected_intervals(y_data_3, "nullplot.jpg") #We expect to fail to reject H_0
+
+a_t_set_1[which.min(a_t_set_1$u),]
+
+
+
+
+
+
+
+
+
+
+
