ba4832f2a06841ad875c32516cd62244c680ef42
Marina Khismatullina <marina.k@uni-bonn.de>
2018-03-14 17:14:22 +0100

    Working version for Section4, still with the same sigma

---
+++ Section_4/simulations_based_on_data.R
@@ -0,0 +1,133 @@
+dyn.load("C_code/psihat_statistic_ij.dll")
+source("C_code/psihat_statistic_ij.R")
+source("C_code/estimating_sigma.R")
+dyn.load("C_code/estimating_sigma.dll")
+source("functions.R")
+
+##############################
+#Defining necessary constants#
+##############################
+
+N     <- 3 #number of different time series
+N_rep <- 1000
+
+different_T     <- c(250, 350, 500, 1000, 2000) #Different lengths of time series for which we calculate size and power
+different_alpha <- c(0.01, 0.05, 0.1) #Different alpha for which we calculate size and power
+
+kernel_f <- "epanechnikov" #Only "epanechnikov" and "biweight" kernel functions are currently supported
+a_hat <- 0.5
+T_sim <- 430
+
+if (kernel_f == "epanechnikov"){
+  kernel_ind = 1
+} else if (kernel_f == "biweight"){
+  kernel_ind = 2
+} else {
+  print('Currently only Epanechnikov and Biweight kernel functions are supported')
+}
+
+
+#Tuning parameters
+L1 <- floor(sqrt(T_sim))
+L2 <- floor(2 * sqrt(T_sim))
+
+#########################################
+#Calculating gaussian quantile for T_sim#
+#########################################
+
+#g_t_set_sim <- creating_g_set(T_sim)
+#gaussian_quantile <- calculating_gaussian_quantile_ij(T_sim, N, g_t_set_sim, kernel_ind, alpha)
+
+######################################
+#Simulating the data for the iid case#
+######################################
+
+#m = numeric(T_sim)
+#for (i in 1:T_sim) {if (i/T_sim < 0.5) {m[i] = -5} else {m[i] = 5}}
+
+
+#iid case
+size_iid <- c()
+
+for (T_size in different_T){
+  simulated_data <- matrix(NA, nrow = T_size, ncol = N)
+  colnames(simulated_data) <- 1:N
+  L1 <- floor(sqrt(T_size))
+  L2 <- floor(2 * sqrt(T_size))
+  g_t_set_sim = creating_g_set(T_size)
+  
+  for (alpha in different_alpha){
+    #Calculating gaussian quantiles for given T and alpha
+    gaussian_quantile = calculating_gaussian_quantile_ij(T_size, N, g_t_set_sim, kernel_ind, alpha)
+    
+    size_of_the_test = replicate(N_rep, {
+      sigmahat_vector_2 <- c()
+      for (i in 1:N){
+        simulated_data[, i] <- rnorm(T_size, 0, 1)
+        sigma_i = estimating_sigma_for_AR1(simulated_data[, i], L1, L2)[[1]]
+        sigmahat_vector_2 <- c(sigmahat_vector_2, sigma_i * sigma_i)
+      }
+      sigmahat <- sqrt(sum(sigmahat_vector_2)/N)
+      
+      matrix_of_statistic <- matrix(, nrow = N, ncol = N)
+      for (i in (1 : (N - 1))){
+        for (j in ((i + 1):N)){
+          result = psihat_statistic_ij(simulated_data[, i], simulated_data[, j], g_t_set_sim, kernel_ind, sigmahat)
+          matrix_of_statistic[i, j] = result[[2]]
+          #      cat(matrix_of_statistic[i, j], "with i =", i, "and j = ",j, "\n")
+        }
+      }
+      result_with_trend = max(as.vector(matrix_of_statistic), na.rm=TRUE)
+      if (result_with_trend > gaussian_quantile) {d = 1} else {d = 0}
+      d
+    })
+    size_iid = c(size_iid, sum(size_of_the_test)/N_rep)
+    cat(sum(size_of_the_test)/N_rep, "with alpha = ", alpha, "and T = ", T_size, "\n")
+  }
+}
+
+
+######################################
+#Simulating the data for the AR1 case#
+######################################
+
+#AR1 case
+size_ar1 <- c()
+
+for (T_size in different_T){
+  simulated_data <- matrix(NA, nrow = T_size, ncol = N)
+  colnames(simulated_data) <- 1:N
+  L1 <- floor(sqrt(T_size))
+  L2 <- floor(2 * sqrt(T_size))
+  g_t_set_sim = creating_g_set(T_size)
+  
+  for (alpha in different_alpha){
+    #Calculating gaussian quantiles for given T and alpha
+    gaussian_quantile = calculating_gaussian_quantile_ij(T_size, N, g_t_set_sim, kernel_ind, alpha)
+    
+    size_of_the_test = replicate(N_rep, {
+      sigmahat_vector_2 <- c()
+      for (i in 1:N){
+        simulated_data[, i] <- arima.sim(model = list(ar = a_hat), innov = rnorm(T_size, 0, 1), n = T_size)
+        sigma_i = estimating_sigma_for_AR1(simulated_data[, i], L1, L2)[[1]]
+        sigmahat_vector_2 <- c(sigmahat_vector_2, sigma_i * sigma_i)
+      }
+      sigmahat <- sqrt(sum(sigmahat_vector_2)/N)
+      
+      matrix_of_statistic <- matrix(, nrow = N, ncol = N)
+      for (i in (1 : (N - 1))){
+        for (j in ((i + 1):N)){
+          result = psihat_statistic_ij(simulated_data[, i], simulated_data[, j], g_t_set_sim, kernel_ind, sigmahat)
+          matrix_of_statistic[i, j] = result[[2]]
+          #      cat(matrix_of_statistic[i, j], "with i =", i, "and j = ",j, "\n")
+        }
+      }
+      result_with_trend = max(as.vector(matrix_of_statistic), na.rm=TRUE)
+      if (result_with_trend > gaussian_quantile) {d = 1} else {d = 0}
+      d
+    })
+    size_ar1 = c(size_ar1, sum(size_of_the_test)/N_rep)
+    cat(sum(size_of_the_test)/N_rep, "with alpha = ", alpha, "and T = ", T_size, "\n")
+  }
+}
+
--- Section_4/testing_different_time_trends.R
+++ Section_4/testing_different_time_trends.R
@@ -1,7 +1,5 @@
 dyn.load("C_code/psihat_statistic_ij.dll")
 source("C_code/psihat_statistic_ij.R")
-#dyn.load("C_code/psihat_statistic.dll")
-#source("C_code/psihat_statistic.R")
 source("C_code/estimating_sigma.R")
 dyn.load("C_code/estimating_sigma.dll")
 source("functions.R")
@@ -11,7 +9,7 @@ source("functions.R")
 ##############################
 
 p <- 1 #Order of AR(p) process of the error terms. Currently only p=1 is supported
-N <- 4 #number of different time series 
+N <- 3 #number of different time series 
 
 alpha    <- 0.05
 kernel_f <- "epanechnikov" #Only "epanechnikov" and "biweight" kernel functions are currently supported
@@ -29,7 +27,7 @@ if (kernel_f == "epanechnikov"){
 ###########################################
 
 for (i in 1:N){
-  filename = paste("data/", i, "_station.txt", sep = "")
+  filename = paste("data/", i, "_station_test.txt", sep = "")
   temperature_tmp  <- read.table(filename, header = FALSE, skip = 7,
                                  col.names = c("year", "month", "tmax", "tmin", "af", "rain", "sun", "aux"), fill = TRUE,  na.strings = c("---"))
   monthly_temp_tmp <- data.frame('1' = temperature_tmp[['year']], '2' = temperature_tmp[['month']],
@@ -42,12 +40,27 @@ for (i in 1:N){
   }
 }
 
+# for (i in 1:N){
+#   filename = paste("data/", i, "_station.txt", sep = "")
+#   temperature_tmp  <- read.table(filename, header = FALSE, skip = 7,
+#                                  col.names = c("year", "month", "tmax", "tmin", "af", "rain", "sun", "aux"), fill = TRUE,  na.strings = c("---"))
+#   monthly_temp_tmp <- data.frame('1' = temperature_tmp[['year']], '2' = temperature_tmp[['month']],
+#                                  '3' = (temperature_tmp[["tmax"]] + temperature_tmp[["tmin"]]) / 2)
+#   colnames(monthly_temp_tmp) <- c('year', 'month', paste0("tmean", i))
+#   if (i == 1){
+#     monthly_temp <- monthly_temp_tmp
+#   } else {
+#     monthly_temp <- merge(monthly_temp, monthly_temp_tmp, by = c("year", "month"))
+#   }
+# }
+
 monthly_temp <- na.omit(monthly_temp)#Deleting the rows with ommitted variables
 T_tempr <- nrow(monthly_temp)
 
-plot(monthly_temp[["tmean1"]],type = 'l')
-plot(monthly_temp[["tmean2"]],type = 'l')
-plot(monthly_temp[["tmean3"]],type = 'l')
+par(mfrow = c(1, 2))
+plot(monthly_temp[["tmean1"]], ylim = c(-3, 3), type = 'l')
+plot(monthly_temp[["tmean2"]],ylim = c(-3, 3),type = 'l')
+#plot(monthly_temp[["tmean3"]],type = 'l')
 
 
 ######################
@@ -77,14 +90,13 @@ for (i in TemperatureColumns){
 
 sigmahat_tempr <- sqrt(sum(sigmahat_vector_2)/N)
 
+
 ###########################################
 #Calculating gaussian quantile for T_tempr#
 ###########################################
 
 g_t_set_tempr <- creating_g_set(T_tempr)
-
-gaussian_quantile_ij <- calculating_gaussian_quantile_ij(T_tempr, N, g_t_set_tempr, kernel_ind, alpha)
-gaussian_quantile <- calculating_gaussian_quantile(T_tempr, N, g_t_set_tempr, kernel_ind, alpha)
+gaussian_quantile <- calculating_gaussian_quantile_ij(T_tempr, N, g_t_set_tempr, kernel_ind, alpha)
 
 
 #########################################
@@ -102,10 +114,9 @@ for (i in (1 : (N - 1))){
     #cat(head(monthly_temp[[j + 2]]), "\n")
     }
 }
+
 #max(vector_of_ij_statistic)
 
-#statistic <- psihat_statistic(monthly_temp, N, g_t_set_tempr, kernel_ind, sigmahat_tempr)
-#result <- psihat_statistic_ij(monthly_temp, g_t_set, kernel_ind, sigmahat_tempr)
 
 
 
