5dac5aafc1b7a6239b9f1599244421823e536ad9
Marina Khismatullina <marina.k@uni-bonn.de>
2018-04-18 17:10:53 +0200

    Started changing testing_differnt_time_trends

--- Section_4/main_section4.R
+++ Section_4/main_section4.R
@@ -18,7 +18,7 @@ source("simulations_based_on_data.R")
 
 N_ts     <- 34 #number of different time series 
 N_rep    <- 1000 #number of repetitions for calculating size and power
-alpha    <- 0.05 #alpha for calculating quantiles
+#alpha    <- 0.05 #alpha for calculating quantiles
 
 different_T     <- c(250, 300, 500, 1000) #Different lengths of time series for which we calculate size and power
 different_alpha <- c(0.01, 0.05, 0.1) #Different alpha for which we calculate size and power
@@ -29,97 +29,94 @@ kernel_method <- "ll" #Only "nw" (Nadaraya-Watson) and "ll" (local linear) metho
 ###########################################
 #Loading the real station data for England#
 ###########################################
-
-for (i in 1:N_ts){
-  filename = paste("data/txt", i, ".txt", sep = "")
-  temperature_tmp  <- read.table(filename, header = FALSE, skip = 7,
-                                 col.names = c("year", "month", "tmax", "tmin", "af", "rain", "sun", "aux"), fill = TRUE,  na.strings = c("---"))
-  monthly_temp_tmp <- data.frame('1' = temperature_tmp[['year']], '2' = temperature_tmp[['month']],
-                                 '3' = (temperature_tmp[["tmax"]] + temperature_tmp[["tmin"]]) / 2)
-  colnames(monthly_temp_tmp) <- c('year', 'month', paste0("tmean", i))
-
-
-  if (i == 1){
-    monthly_temp <- monthly_temp_tmp
-  } else {
-    monthly_temp <- merge(monthly_temp, monthly_temp_tmp, by = c("year", "month"))
-  }
-
-}
-
-monthly_temp <- na.omit(monthly_temp)#Deleting the rows with ommitted variables
-date         <- paste(sprintf("%02d", monthly_temp$month), monthly_temp$year,  sep='-')
-monthly_temp <- cbind(date, monthly_temp)
-T_tempr      <- nrow(monthly_temp)
-
-
-######################
-#Deseasonalizing data#
-######################
-
-TemperatureColumns <- setdiff(names(monthly_temp), c("year", "month", "date"))
-for (i in TemperatureColumns){
-  monthly_temp[[i]] <- monthly_temp[[i]] - ave(monthly_temp[[i]], monthly_temp[['month']], FUN = mean)
-  #plot(monthly_temp[[i]],type = 'l')
-}
-
-
-###################################################################
-#Calculating smoothed curve for the data using Epanechnikov kernel#
-###################################################################
-
-grid_points <- seq(from = 1/T_tempr, to = 1, length.out = T_tempr) #grid points for estimating
-
-pdf("../Plots/stations_data.pdf", width=10, height=10, paper="special")
-par(mfrow = c(3,1), cex = 1.1, tck = -0.025) #Setting the layout of the graphs
-par(mar = c(0, 0.5, 0, 0)) #Margins for each plot
-par(oma = c(2.5, 1.5, 0.2, 0.2)) #Outer margins
-
-plot(NA, ylab="", xlab = "", xlim = c(0,1), ylim = c(-1.5, 1.5), yaxp  = c(-1.0, 1.0, 2), xaxt = 'n', mgp=c(2,0.5,0), cex = 1.2, tck = -0.025)
-for (column in TemperatureColumns){
-  smoothed_curve <- mapply(local_linear_smoothing, grid_points, MoreArgs = list(monthly_temp[[column]], grid_points, 0.05))
-  lines(grid_points, smoothed_curve)#, lty = i), col = colors[i]) 
-}
-axis(1, at = grid_points[seq(1, 300, by = 20)], labels = monthly_temp$date[seq(1, 300, by = 20)])
-legend(0, 1.0, legend=c("h = 0.05"), lty = 1, cex = 0.95, ncol=1)
-
-plot(NA, ylab="", xlab = "", xlim = c(0,1), ylim = c(-1.5, 1.5), yaxp  = c(-1.0, 1.0, 2),xaxt = 'n', mgp=c(2,0.5,0), cex = 1.2, tck = -0.025)
-for (column in TemperatureColumns){
-  smoothed_curve <- mapply(local_linear_smoothing, grid_points, MoreArgs = list(monthly_temp[[column]], grid_points, 0.1))
-  lines(grid_points, smoothed_curve)#, lty = i), col = colors[i]) 
-}
-axis(1, at = grid_points[seq(1, 300, by = 20)], labels = monthly_temp$date[seq(1, 300, by = 20)])
-legend(0, 1.0, legend=c("h = 0.10"), lty = 1, cex = 0.95, ncol=1)
-
-
-plot(NA, ylab="", xlab = "", xlim = c(0,1), ylim = c(-1.5, 1.5), yaxp  = c(-1.0, 1.0, 2),xaxt = 'n', mgp=c(2,0.5,0), cex = 1.2, tck = -0.025)
-for (column in TemperatureColumns){
-  smoothed_curve <- mapply(local_linear_smoothing, grid_points, MoreArgs = list(monthly_temp[[column]], grid_points, 0.15))
-  lines(grid_points, smoothed_curve)#, lty = i), col = colors[i]) 
-}
-axis(1, at = grid_points[seq(1, 300, by = 20)], labels = monthly_temp$date[seq(1, 300, by = 20)])
-legend(0, 1.0, legend=c("h = 0.15"), lty = 1, cex = 0.95, ncol=1)
-
-dev.off()
-
+# 
+# for (i in 1:N_ts){
+#   filename = paste("data/txt", i, ".txt", sep = "")
+#   temperature_tmp  <- read.table(filename, header = FALSE, skip = 7,
+#                                  col.names = c("year", "month", "tmax", "tmin", "af", "rain", "sun", "aux"), fill = TRUE,  na.strings = c("---"))
+#   monthly_temp_tmp <- data.frame('1' = temperature_tmp[['year']], '2' = temperature_tmp[['month']],
+#                                  '3' = (temperature_tmp[["tmax"]] + temperature_tmp[["tmin"]]) / 2)
+#   colnames(monthly_temp_tmp) <- c('year', 'month', paste0("tmean", i))
+# 
+# 
+#   if (i == 1){
+#     monthly_temp <- monthly_temp_tmp
+#   } else {
+#     monthly_temp <- merge(monthly_temp, monthly_temp_tmp, by = c("year", "month"))
+#   }
+# 
+# }
+# 
+# monthly_temp <- na.omit(monthly_temp)#Deleting the rows with ommitted variables
+# date         <- paste(sprintf("%02d", monthly_temp$month), monthly_temp$year,  sep='-')
+# monthly_temp <- cbind(date, monthly_temp)
+# T_tempr      <- nrow(monthly_temp)
+# 
+# 
+# ######################
+# #Deseasonalizing data#
+# ######################
+# 
+# TemperatureColumns <- setdiff(names(monthly_temp), c("year", "month", "date"))
+# for (i in TemperatureColumns){
+#   monthly_temp[[i]] <- monthly_temp[[i]] - ave(monthly_temp[[i]], monthly_temp[['month']], FUN = mean)
+#   #plot(monthly_temp[[i]],type = 'l')
+# }
+# 
+# 
+# ###################################################################
+# #Calculating smoothed curve for the data using Epanechnikov kernel#
+# ###################################################################
+# 
+# grid_points <- seq(from = 1/T_tempr, to = 1, length.out = T_tempr) #grid points for estimating
+# 
+# pdf("../Plots/stations_data.pdf", width=10, height=10, paper="special")
+# par(mfrow = c(3,1), cex = 1.1, tck = -0.025) #Setting the layout of the graphs
+# par(mar = c(0, 0.5, 0, 0)) #Margins for each plot
+# par(oma = c(2.5, 1.5, 0.2, 0.2)) #Outer margins
+# 
+# plot(NA, ylab="", xlab = "", xlim = c(0,1), ylim = c(-1.5, 1.5), yaxp  = c(-1.0, 1.0, 2), xaxt = 'n', mgp=c(2,0.5,0), cex = 1.2, tck = -0.025)
+# for (column in TemperatureColumns){
+#   smoothed_curve <- mapply(local_linear_smoothing, grid_points, MoreArgs = list(monthly_temp[[column]], grid_points, 0.05))
+#   lines(grid_points, smoothed_curve)#, lty = i), col = colors[i]) 
+# }
+# axis(1, at = grid_points[seq(1, 300, by = 20)], labels = monthly_temp$date[seq(1, 300, by = 20)])
+# legend(0, 1.0, legend=c("h = 0.05"), lty = 1, cex = 0.95, ncol=1)
+# 
+# plot(NA, ylab="", xlab = "", xlim = c(0,1), ylim = c(-1.5, 1.5), yaxp  = c(-1.0, 1.0, 2),xaxt = 'n', mgp=c(2,0.5,0), cex = 1.2, tck = -0.025)
+# for (column in TemperatureColumns){
+#   smoothed_curve <- mapply(local_linear_smoothing, grid_points, MoreArgs = list(monthly_temp[[column]], grid_points, 0.1))
+#   lines(grid_points, smoothed_curve)#, lty = i), col = colors[i]) 
+# }
+# axis(1, at = grid_points[seq(1, 300, by = 20)], labels = monthly_temp$date[seq(1, 300, by = 20)])
+# legend(0, 1.0, legend=c("h = 0.10"), lty = 1, cex = 0.95, ncol=1)
+# 
+# 
+# plot(NA, ylab="", xlab = "", xlim = c(0,1), ylim = c(-1.5, 1.5), yaxp  = c(-1.0, 1.0, 2),xaxt = 'n', mgp=c(2,0.5,0), cex = 1.2, tck = -0.025)
+# for (column in TemperatureColumns){
+#   smoothed_curve <- mapply(local_linear_smoothing, grid_points, MoreArgs = list(monthly_temp[[column]], grid_points, 0.15))
+#   lines(grid_points, smoothed_curve)#, lty = i), col = colors[i]) 
+# }
+# axis(1, at = grid_points[seq(1, 300, by = 20)], labels = monthly_temp$date[seq(1, 300, by = 20)])
+# legend(0, 1.0, legend=c("h = 0.15"), lty = 1, cex = 0.95, ncol=1)
+# 
+# dev.off()
+# 
 
 #####################
 #Estimating variance#
 #####################
 
 #Tuning parameters
-L1 <- floor(sqrt(T_tempr))
-L2 <- floor(2 * sqrt(T_tempr))
-
-#Calculating each sigma_i separately
-sigmahat_vector_2 <- c()
-for (i in TemperatureColumns){
-  sigma_i <- estimating_sigma_for_AR1(monthly_temp[[i]], L1, L2)[[1]]
-  sigmahat_vector_2 <- c(sigmahat_vector_2, sigma_i * sigma_i)
-}
-
-#Calculating sqrt root of the average long-run variance
-#sigmahat_tempr <- sqrt(sum(sigmahat_vector_2)/N)
+# L1 <- floor(sqrt(T_tempr))
+# L2 <- floor(2 * sqrt(T_tempr))
+# 
+# #Calculating each sigma_i separately
+# sigmahat_vector_2 <- c()
+# for (i in TemperatureColumns){
+#   sigma_i <- estimating_sigma_for_AR1(monthly_temp[[i]], L1, L2)[[1]]
+#   sigmahat_vector_2 <- c(sigmahat_vector_2, sigma_i * sigma_i)
+# }
 
 
 #################################
@@ -134,7 +131,7 @@ for (i in TemperatureColumns){
 ############################
 
 source("simulations_based_on_data.R")
-results_size     <- simulations_size(15, N_rep, different_T, different_alpha, kernel_method)
+#results_size     <- simulations_size(15, N_rep, different_T, different_alpha, kernel_method)
 #results_power    <- simulations_power(15, N_rep, different_T, different_alpha, kernel_method)
-#results_clusters <- simulations_clustering(15, N_rep, different_T, different_alpha, kernel_method)
+results_clusters <- simulations_clustering(15, N_rep, different_T, different_alpha, kernel_method)
 
--- Section_4/testing_different_time_trends.R
+++ Section_4/testing_different_time_trends.R
@@ -1,40 +1,63 @@
-testing_different_time_trends <- function(N, y_data, alpha, kernel_method, sigmahat_vector_2){
+testing_different_time_trends <- function(N_ts, y_data, alpha, kernel_method, sigmahat_vector_2){
   
   T_data <- nrow(y_data)
-  
+
   ###########################################
   #Calculating gaussian quantile for T_tempr#
   ###########################################
   
   g_t_set                    <- creating_g_set(T_data, kernel_method)
-  gaussian_quantile          <- calculating_gaussian_quantile(T_data, N, g_t_set, kernel_method, alpha)
-  pairwise_gaussian_quantile <- calculating_gaussian_quantile(T_data, 2, g_t_set, kernel_method, alpha)
-
+  
+  filename = paste("distribution/distr_for_application_T_", T, "_and_N_ts_", N_ts, "_and_method_", kernel_method, ".RData", sep = "")
+  if(!file.exists(filename)) {
+    gaussian_statistic_distribution <- replicate(1000, {
+      z_matrix <- matrix(, nrow = T_data, ncol = N_ts)
+      for (i in 1:N_ts){
+        z_matrix[, i] <- rnorm(T_data, 0, sqrt(sigmahat_vector_2[i]))
+      }
+      psistar_statistic(z_matrix, N_ts, g_t_set, sigmahat_vector_2, kernel_method)
+    })
+    save(gaussian_statistic_distribution, file = filename)
+  } else {
+    load(filename)
+  }
+  gaussian_quantile <- quantile(gaussian_statistic_distribution, probs = (1 - alpha), type = 1)  
+  cat("Gaussian quantile is", gaussian_quantile, "\n")
+  
+  filename = paste("distribution/pairwise_distr_for_application_T_", T, "_and_method_", kernel_method, ".RData", sep = "")
+  if(!file.exists(filename)) {
+    pairwise_gaussian_statistic_distribution <- replicate(1000, {
+      z_matrix <- matrix(, nrow = T_data, ncol = 2)
+      for (i in 1:2){
+        z_matrix[, i] <- rnorm(T_data, 0, sqrt(sigmahat_vector_2[i]))
+      }
+      psistar_statistic(z_matrix, N_ts, g_t_set, sigmahat_vector_2, kernel_method)
+    })
+    save(pairwise_gaussian_statistic_distribution, file = filename)
+  } else {
+    load(filename)
+  }
+  
+  pairwise_gaussian_quantile <- quantile(pairwise_gaussian_statistic_distribution, probs = (1 - alpha), type = 1) 
+  cat("Pairwise gaussian quantile is", pairwise_gaussian_quantile, "\n")
+  
   #########################################
   #Calculating the statistic for real data#
   #########################################
   
-  #Calculating sqrt root of the average long-run variance
-  sigmahat_tempr <- sqrt(sum(sigmahat_vector_2)/N)
-  
   matrix_of_statistic <- matrix(, nrow = N, ncol = N)
-  matrix_of_statistic_new <- matrix(, nrow = N, ncol = N)
   for (i in (1 : (N - 1))){
     for (j in ((i + 1):N)){
-      sigmahat_new = sqrt(sigmahat_vector_2[i] + sigmahat_vector_2[j])
-      result = psihat_statistic_ij(y_data[[i]], y_data[[j]], g_t_set, sqrt(2) * sigmahat_tempr, kernel_method)
-      result_new = psihat_statistic_ij(y_data[[i]], y_data[[j]], g_t_set, sigmahat_new, kernel_method)
+      sigmahat = sqrt(sigmahat_vector_2[i] + sigmahat_vector_2[j])
+      result = psihat_statistic_ij(y_data[[i]], y_data[[j]], g_t_set, sigmahat, kernel_method)
       matrix_of_statistic[i, j] = result[[2]]
-      matrix_of_statistic_new[i, j] = result_new[[2]]
       cat(matrix_of_statistic[i, j], "with i =", i, "and j = ",j, "\n")
-      #g_t_set = result[[1]]
     }
   }
   statistic = max(as.vector(matrix_of_statistic), na.rm=TRUE)
-  statistic_new = max(as.vector(matrix_of_statistic_new), na.rm=TRUE)
-  
+
   #And now the testing itself
-  if (statistic_new > gaussian_quantile) {
+  if (statistic > gaussian_quantile) {
     cat("We reject H_0 with probability", alpha, "Psihat_statistic = ", statistic_new,
         "Gaussian quantile value = ", gaussian_quantile, "\n")
   } else {
@@ -42,13 +65,5 @@ testing_different_time_trends <- function(N, y_data, alpha, kernel_method, sigma
         "Gaussian quantile value = ", gaussian_quantile, "\n")
   }
   
-  if (statistic > gaussian_quantile) {
-    cat("For old sigma estimate: We reject H_0 with probability", alpha, "Psihat_statistic = ", statistic,
-        "Gaussian quantile value = ", gaussian_quantile, "\n")
-  } else {
-    cat("For old sigma estimate: We fail to reject H_0 with probability", alpha, "Psihat_statistic = ", statistic,
-        "Gaussian quantile value = ", gaussian_quantile, "\n")
-  }
-  
-  return(list(matrix_of_statistic, matrix_of_statistic_new, gaussian_quantile, pairwise_gaussian_quantile))
+  return(list(matrix_of_statistic, gaussian_quantile, pairwise_gaussian_quantile))
 }
