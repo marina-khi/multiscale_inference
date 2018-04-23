d0c1b7fb6c3045fdd24bd0ccd5d65e66874b31c7
MKhismatullina <marina.k@uni-bonn.de>
2018-04-23 20:29:38 +0200

    Some changes regarding the application

--- Section_4/testing_different_time_trends.R
+++ Section_4/testing_different_time_trends.R
@@ -11,7 +11,7 @@ testing_different_time_trends <- function(N_ts, y_data, alpha, kernel_method, si
   filename = paste("distribution/distr_for_application_T_", T, "_and_N_ts_", N_ts, "_and_method_", kernel_method, ".RData", sep = "")
   if(!file.exists(filename)) {
     gaussian_statistic_distribution <- replicate(1000, {
-      z_matrix <- matrix(, nrow = T_data, ncol = N_ts)
+      z_matrix <- matrix(0, nrow = T_data, ncol = N_ts)
       for (i in 1:N_ts){
         z_matrix[, i] <- rnorm(T_data, 0, sqrt(sigmahat_vector_2[i]))
       }
@@ -27,7 +27,7 @@ testing_different_time_trends <- function(N_ts, y_data, alpha, kernel_method, si
   filename = paste("distribution/pairwise_distr_for_application_T_", T, "_and_method_", kernel_method, ".RData", sep = "")
   if(!file.exists(filename)) {
     pairwise_gaussian_statistic_distribution <- replicate(1000, {
-      z_matrix <- matrix(, nrow = T_data, ncol = 2)
+      z_matrix <- matrix(0, nrow = T_data, ncol = 2)
       for (i in 1:2){
         z_matrix[, i] <- rnorm(T_data, 0, sqrt(sigmahat_vector_2[i]))
       }
@@ -45,17 +45,11 @@ testing_different_time_trends <- function(N_ts, y_data, alpha, kernel_method, si
   #Calculating the statistic for real data#
   #########################################
   
-  matrix_of_statistic <- matrix(, nrow = N, ncol = N)
-  for (i in (1 : (N - 1))){
-    for (j in ((i + 1):N)){
-      sigmahat = sqrt(sigmahat_vector_2[i] + sigmahat_vector_2[j])
-      result = psihat_statistic_ij(y_data[[i]], y_data[[j]], g_t_set, sigmahat, kernel_method)
-      matrix_of_statistic[i, j] = result[[2]]
-      cat(matrix_of_statistic[i, j], "with i =", i, "and j = ",j, "\n")
-    }
-  }
-  statistic = max(as.vector(matrix_of_statistic), na.rm=TRUE)
-
+  statistic_matrix <- matrix(0, nrow = N_ts, ncol = N_ts)
+  results          <- psihat_statistic(y_data, N_ts, g_t_set, sigmahat_vector_2, kernel_method)
+  statistic        <- result[[2]]
+  statistic_matrix[lower.tri(statistic_matrix, diag=FALSE)] <- result[[1]]
+  
   #And now the testing itself
   if (statistic > gaussian_quantile) {
     cat("We reject H_0 with probability", alpha, "Psihat_statistic = ", statistic_new,
