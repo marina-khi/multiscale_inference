9e1b72e2bdfa7ed6a470e601a89923248cf25d59
Marina <marina.k@uni-bonn.de>
2018-09-12 18:09:02 +0200

    Normal implementation of SiZer simulations

--- One_trend/Shape/SiZer_simulations.R
+++ One_trend/Shape/SiZer_simulations.R
@@ -2,7 +2,12 @@ library(xtable)
 library(matlib)
 options(xtable.floating = FALSE)
 options(xtable.timestamp = "")
+
 source("Shape/functions.R")
+source("Shape/C_code/estimating_sigma.R")
+dyn.load("Shape/C_code/estimating_sigma.dll")
+dyn.load("Shape/C_code/psihat_statistic_ll.dll")
+source("Shape/C_code/psihat_statistic.R")
 
 ############################################
 #These are all the SiZer-specific functions#
@@ -17,17 +22,6 @@ estimating_variance_of_data <- function(data){
   return(result)
 }
 
-#Implementation of calculating q(h) from "SiZer for time series" paper
-gaussian_quantile_for_sizer <- function(i_0, h, y_data, var_epsilon, var_y_bar, ESS, alpha){
-  T_data   <- length(y_data)
-  T_star   <- var_epsilon/var_y_bar
-  ESS_star <- (T_star/T_data) * ESS 
-  l        <- T_data / ESS
-  return(list(qnorm((1 + (1 - alpha)^(1 / l))/2), ESS_star))
-}
-
-
-
 #Calculate autocovariance function for AR(1) model \varepsilon_t = a_1 \varepsilon_{t-1} + \eta_t based on the coefficients of the model
 autocovariance_function_AR1 <- function(k, a_1, sigma_eta){
   if (k%%1==0)
@@ -39,87 +33,133 @@ autocovariance_function_AR1 <- function(k, a_1, sigma_eta){
   return(result)
 }
 
-calculating_estimator_and_variance <- function(T_size, i, h, gamma, data){
-  x_matrix <- matrix(c(rep(1, T_size), seq(1/T_size - i, 1-i, by = 1/T_size)), nrow=T_size, byrow=FALSE)
-  XtSigma  <- matrix(data = NA, nrow =2, ncol =T_size)
-  w_vector <- sapply(seq(1/T_size - i, 1-i, by = 1/T_size)/h, FUN = epanechnikov_kernel)/h
-  
-  for (j in 1:T_size){      #t(X) %*% Sigma computed faster in order not to save the whole Sigma matrix (T_size * T_size) in the memory
-    XtSigma[1, j] = 0
-    XtSigma[2, j] = 0
-    for (k in 1:T_size){
-      XtSigma[1, j] = XtSigma[1, j] + gamma[abs(k - j) + 1] * w_vector[k] * w_vector[j]
-      XtSigma[2, j] = XtSigma[2, j] + gamma[abs(k - j) + 1] * w_vector[k] * w_vector[j] * (k/T_size - i)   
-    }
-  }
-
-  XtW            = t(apply(x_matrix,2,function(x){x*w_vector}))# t(X) %*% diag(w)   computed faster.  :)
-  inverse_matrix = tryCatch({solve(XtW %*% x_matrix)},  
-                            error = function(e) {print("Something is wrong, the matrix can not be inverted")})
-
-  variances_beta = diag(inverse_matrix %*% XtSigma %*% x_matrix %*% inverse_matrix)
-  estimator      = inverse_matrix %*% XtW %*% data
-  
-  return(list(estimator, variances_beta))
-}
-
-SiZer_single_estimation <- function(i, h, y_data, gamma_estimated, q_h){
+SiZer_single_estimation <- function(i, h, y_data, gamma_estimated, q_h, X_matrix, W_vector, XtW, XtWX_inv){
   T_data <- length(y_data)
   
-  result         <- calculating_estimator_and_variance(T_data, i, h, gamma_estimated, y_data)
+  result         <- calculating_estimator_and_variance(i, h, gamma_estimated, y_data, X_matrix, W_vector, XtW, XtWX_inv)
   m_hat_prime    <- result[[1]][2, 1]
   sd_m_hat_prime <- sqrt(result[[2]][2])
 
-  if (m_hat_prime - q_h * sd_m_hat_prime > 0){
-    result = 1
-  } else if (m_hat_prime + q_h * sd_m_hat_prime < 0) {
-    result = -1
-  } else {
-    result = 0
-  }
-  #cat("i = ", i, " and h = ", h, ": interval = [", m_hat_prime - q_h * sd_m_hat_prime, ",",m_hat_prime + q_h * sd_m_hat_prime, "]\n")
   return(result)
 }
 ###############################
 #Defining necessary parameters#
 ###############################
 
-a_1       <- 0.6
-sigma_eta <- 1
-T_size    <- 200
-alpha     <- 0.05
-
-####################################
-#Estimating autocovariance function#
-####################################
-
-gamma = c()
-for (k in 0:(T_size-1)){gamma = c(gamma, autocovariance_function_AR1(k, a_1, sigma_eta))}
+a_1         <- 0.5
+sigma_eta   <- 1
+T_size      <- 200
+alpha       <- 0.05
+different_i <- seq(from = 1/T_size, to = 1, by = 1/T_size)
+different_h <- seq(from = 3/T_size, to = 1/4+3/T_size, by = 5/T_size)
+N_rep       <- 100  
 
 i_0  <- round(T_size/2) /T_size #Auxiliary data point to calculate Effective Sample Size
-h    <- 0.1
+L1 = floor(sqrt(T_size))
+L2 = floor(2 * sqrt(T_size))
+kernel_ind = 2
 
-y_data_ar_1       <- arima.sim(model = list(ar = a_1), n = T_size, innov = rnorm(T_size, 0, sigma_eta))
-variance_of_y_bar <- estimating_variance_of_data(y_data_ar_1)
-ESS               <- sum(sapply((i_0 - seq(1/T_size, 1, by = 1/T_size))/h, epanechnikov_kernel))/epanechnikov_kernel(0)
+##################################################
+#Calculating everything that does not depend on Y#
+##################################################
+gamma = c()
+for (k in 0:(T_size-1)){                                            #gamma(k) = sigma_eta^2 * a_1^|k| / (1 - a_1^2)
+  gamma = c(gamma, autocovariance_function_AR1(k, a_1, sigma_eta))
+}
 
-g_t_set        <- creating_g_set(T_size, "nw") #Here we use NW method to create the set of locations and bandwidth because we want the restriction [u-h, u+h] \in [0,1] 
-g_t_set$result <- numeric(nrow(g_t_set)) # Setting the results of SiZer method to be zero for each location and bandwidth
-
+#Calculating \Var(\bar{Y}) based on the true values of gamma(k)
 true_var <- autocovariance_function_AR1(0, a_1, sigma_eta) / T_size
 for (k in 1:(T_size-1)){true_var = true_var + (2/T_size) * (1 - k/T_size) * autocovariance_function_AR1(k, a_1, sigma_eta)}
 
+T_star   <- gamma[1]/true_var
 
-system.time({
-  result_2 <- gaussian_quantile_for_sizer(i_0, h, y_data_ar_1, gamma[1], variance_of_y_bar, ESS, alpha) #Here we estimate sigma^2 (variance of epsilon from SiZer paper) by \widehat{gamma}(0)  
-  q_h      <- result_2[[1]]
-  ESS_star <- result_2[[2]]
-  if (ESS_star >= 5){
-    a <- sapply(seq(1/T_size, 1, by = 1/T_size), FUN = SiZer_single_estimation, h, y_data_ar_1, gamma, q_h)
+SiZer_matrix          <- expand.grid(u = different_i, h = different_h) #Creating a dataframe with all possible combination of i and h
+SiZer_matrix$values   <- numeric(nrow(SiZer_matrix)) # Setting the values of the statistic to be zero
+SiZer_matrix$sd       <- numeric(nrow(SiZer_matrix)) # Setting the values of standard deviation to be zero
+SiZer_matrix$ESS_star <- numeric(nrow(SiZer_matrix)) # Setting the values of ESS* to be zero
+SiZer_matrix$q_h      <- numeric(nrow(SiZer_matrix)) # Setting the values of the gaussian quantile to be zero
+SiZer_matrix$small_ESS<- numeric(nrow(SiZer_matrix)) # Later we will delete all the row such that ESS* is too small
+SiZer_matrix$lambda   <- lambda(SiZer_matrix[['h']]) #Calculating the lambda(h) in order to speed up the function psistar_statistic
+
+
+for (row in 1:nrow(SiZer_matrix)){
+  
+  i = SiZer_matrix[row, 'u']
+  h = SiZer_matrix[row, 'h']
+  
+  ESS      <- sum(sapply((i - seq(1/T_size, 1, by = 1/T_size))/h, epanechnikov_kernel))/epanechnikov_kernel(0)
+  ESS_star <- (T_star/T_size) * ESS 
+  l        <- T_size / ESS_star
+  q_h      <- qnorm((1 + (1 - alpha)^(1 / l))/2)
+  
+  if (ESS_star <= 5){
+    SiZer_matrix[row, 'small_ESS'] <- 1
+  } else {
+    x_matrix     <- matrix(c(rep(1, T_size), seq(1/T_size - i, 1-i, by = 1/T_size)), nrow=T_size, byrow=FALSE)
+    w_vector     <- sapply(seq(1/T_size - i, 1-i, by = 1/T_size)/h, FUN = epanechnikov_kernel)/h
+    XtW          <- t(apply(x_matrix,2,function(x){x*w_vector}))   #t(X) %*% diag(w)   computed faster.  :)
+    XtWX_inverse <- tryCatch({solve(XtW %*% x_matrix)},  
+                            error = function(e) {print("Something is wrong, the matrix can not be inverted")})
+  
+    XtSigma <- matrix(data = NA, nrow =2, ncol =T_size)
+    for (j in 1:T_size){      #t(X) %*% Sigma computed faster in order not to save the whole Sigma matrix (T_size * T_size) in the memory
+      XtSigma[1, j] = 0
+      XtSigma[2, j] = 0
+      for (k in 1:T_size){
+        XtSigma[1, j] = XtSigma[1, j] + gamma[abs(k - j) + 1] * w_vector[k] * w_vector[j]
+        XtSigma[2, j] = XtSigma[2, j] + gamma[abs(k - j) + 1] * w_vector[k] * w_vector[j] * (k/T_size - i)   
+      }
+    }
+    sd = sqrt((XtWX_inverse %*% XtSigma %*% x_matrix %*% XtWX_inverse)[2,2])
+  
+    SiZer_matrix[row, 'sd']       = sd
+    SiZer_matrix[row, 'q_h']      = q_h
+    SiZer_matrix[row, 'ESS_star'] = ESS_star
   }
+}
+
+SiZer_matrix <- SiZer_matrix[SiZer_matrix$small_ESS == 0,]
+
+#Calculating Gaussian statistic for our own method
+gaussian_statistic_distribution <- replicate(1000, {
+  z = rnorm(T_size, 0, 1)
+  psistar_statistic_ll(z, SiZer_matrix, kernel_ind, 1)
 })
+gaussian_quantile <- quantile(gaussian_statistic_distribution, probs = (1 - alpha), type = 1)
 
-#g_t_set$result <- with(g_t_set, SiZer_single_estimation(u, i_0, h, y_data_ar_1, gamma, variance_of_y_bar, ESS, alpha))
+#One run of SiZer
+
+#m = numeric(T_size)
+#for (i in 1:T_size) {if (i/T_size < 0.5) {m[i] = 0} else {m[i] = i - 0.5*T_size}}
+#y <- y_data_ar_1 + m
 
 
+size_of_the_test <- replicate(N_rep, {
+  y_data_ar_1        <- arima.sim(model = list(ar = a_1), n = T_size, innov = rnorm(T_size, 0, sigma_eta))
+  #variance_of_y_bar <- estimating_variance_of_data(y_data_ar_1)
+  results            <- c()
+  for (row in 1:nrow(SiZer_matrix)){
+    i = SiZer_matrix[row, 'u']
+    h = SiZer_matrix[row, 'h']
+    q_h = SiZer_matrix[row, 'q_h']
+    sd_m_hat_prime = SiZer_matrix[row, 'sd']
 
+    x_matrix     <- matrix(c(rep(1, T_size), seq(1/T_size - i, 1-i, by = 1/T_size)), nrow=T_size, byrow=FALSE)
+    w_vector     <- sapply(seq(1/T_size - i, 1-i, by = 1/T_size)/h, FUN = epanechnikov_kernel)/h
+    XtW          <- t(apply(x_matrix,2,function(x){x*w_vector}))   #t(X) %*% diag(w)   computed faster.  :)
+    XtWX_inverse <- tryCatch({solve(XtW %*% x_matrix)},  
+                           error = function(e) {print("Something is wrong, the matrix can not be inverted")})
+  
+    m_hat_prime <- (XtWX_inverse %*% XtW %*% y_data_ar_1)[2]
+  
+    if (m_hat_prime - q_h * sd_m_hat_prime > 0){
+      results = c(results, 1)
+    } else if (m_hat_prime + q_h * sd_m_hat_prime < 0) {
+      results = c(results, -1)
+    } else {
+      results = c(results, 0)
+    }
+  #cat("i = ", i, " and h = ", h, ": interval = [", m_hat_prime - q_h * sd_m_hat_prime, ",",m_hat_prime + q_h * sd_m_hat_prime, "]\n")
+  }
+  results
+})
