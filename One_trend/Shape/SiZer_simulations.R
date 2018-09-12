bd698826e0d4f0d9fde2563525237d52bddbf4ca
MKhismatullina <marina.k@uni-bonn.de>
2018-09-13 00:09:58 +0200

    together with our method

--- One_trend/Shape/SiZer_simulations.R
+++ One_trend/Shape/SiZer_simulations.R
@@ -59,9 +59,9 @@ L1 = floor(sqrt(T_size))
 L2 = floor(2 * sqrt(T_size))
 kernel_ind = 2
 
-##################################################
-#Calculating everything that does not depend on Y#
-##################################################
+############################################################
+#Calculating everything that does not depend on Y for SiZer#
+############################################################
 gamma = c()
 for (k in 0:(T_size-1)){                                            #gamma(k) = sigma_eta^2 * a_1^|k| / (1 - a_1^2)
   gamma = c(gamma, autocovariance_function_AR1(k, a_1, sigma_eta))
@@ -120,6 +120,11 @@ for (row in 1:nrow(SiZer_matrix)){
 
 SiZer_matrix <- SiZer_matrix[SiZer_matrix$small_ESS == 0,]
 
+#################################################################
+#Calculating everything that does not depend on Y for our method#
+#################################################################
+
+
 #Calculating Gaussian statistic for our own method
 gaussian_statistic_distribution <- replicate(1000, {
   z = rnorm(T_size, 0, 1)
@@ -127,22 +132,23 @@ gaussian_statistic_distribution <- replicate(1000, {
 })
 gaussian_quantile <- quantile(gaussian_statistic_distribution, probs = (1 - alpha), type = 1)
 
-#One run of SiZer
 
-#m = numeric(T_size)
-#for (i in 1:T_size) {if (i/T_size < 0.5) {m[i] = 0} else {m[i] = i - 0.5*T_size}}
-#y <- y_data_ar_1 + m
+
 
 
 size_of_the_test <- replicate(N_rep, {
-  y_data_ar_1        <- arima.sim(model = list(ar = a_1), n = T_size, innov = rnorm(T_size, 0, sigma_eta))
-  #variance_of_y_bar <- estimating_variance_of_data(y_data_ar_1)
-  results            <- c()
-  for (row in 1:nrow(SiZer_matrix)){
-    i = SiZer_matrix[row, 'u']
-    h = SiZer_matrix[row, 'h']
-    q_h = SiZer_matrix[row, 'q_h']
-    sd_m_hat_prime = SiZer_matrix[row, 'sd']
+  y_data_ar_1 <- arima.sim(model = list(ar = a_1), n = T_size, innov = rnorm(T_size, 0, sigma_eta))
+  sigmahat    <- estimating_sigma_for_AR1(y_data_ar_1, L1, L2)[[1]]
+  g_t_set     <- psihat_statistic_ll(y_data_ar_1, SiZer_matrix, kernel_ind, sigmahat)[[1]]
+
+  results_our   <- c()
+  results_their <- c()
+  
+  for (row in 1:nrow(g_t_set)){
+    i              = g_t_set[row, 'u']
+    h              = g_t_set[row, 'h']
+    q_h            = g_t_set[row, 'q_h']
+    sd_m_hat_prime = g_t_set[row, 'sd']
 
     x_matrix     <- matrix(c(rep(1, T_size), seq(1/T_size - i, 1-i, by = 1/T_size)), nrow=T_size, byrow=FALSE)
     w_vector     <- sapply(seq(1/T_size - i, 1-i, by = 1/T_size)/h, FUN = epanechnikov_kernel)/h
@@ -153,13 +159,66 @@ size_of_the_test <- replicate(N_rep, {
     m_hat_prime <- (XtWX_inverse %*% XtW %*% y_data_ar_1)[2]
   
     if (m_hat_prime - q_h * sd_m_hat_prime > 0){
-      results = c(results, 1)
+      results_their = c(results_their, 1)
     } else if (m_hat_prime + q_h * sd_m_hat_prime < 0) {
-      results = c(results, -1)
+      results_their = c(results_their, -1)
     } else {
-      results = c(results, 0)
+      results_their = c(results_their, 0)
     }
-  #cat("i = ", i, " and h = ", h, ": interval = [", m_hat_prime - q_h * sd_m_hat_prime, ",",m_hat_prime + q_h * sd_m_hat_prime, "]\n")
+    if (g_t_set[row, 'values_with_sign'] > g_t_set[row, 'lambda'] + gaussian_quantile){
+      results_our = c(results_our, 1)
+    } else if (-g_t_set[row, 'values_with_sign'] > g_t_set[row, 'lambda'] + gaussian_quantile){
+      results_our = c(results_our, -1)
+    } else {
+      results_our = c(results_our, 0)
+    }
+    
+    #cat("i = ", i, " and h = ", h, ": interval = [", m_hat_prime - q_h * sd_m_hat_prime, ",",m_hat_prime + q_h * sd_m_hat_prime, "]\n")
   }
-  results
+  c(sum(abs(results_their)), sum(abs(results_our)))
+})
+
+power_of_the_test <- replicate(N_rep, {
+  y_data_ar_1 <- arima.sim(model = list(ar = a_1), n = T_size, innov = rnorm(T_size, 0, sigma_eta))
+  m = numeric(T_size)
+  for (i in 1:T_size) {m[i] = i - 0.5*T_size}
+  y_data_ar_1_with_trend <- y_data_ar_1 + m
+  sigmahat    <- estimating_sigma_for_AR1(y_data_ar_1_with_trend, L1, L2)[[1]]
+  g_t_set     <- psihat_statistic_ll(y_data_ar_1_with_trend, SiZer_matrix, kernel_ind, sigmahat)[[1]]
+  
+  results_our   <- c()
+  results_their <- c()
+  
+  for (row in 1:nrow(g_t_set)){
+    i              = g_t_set[row, 'u']
+    h              = g_t_set[row, 'h']
+    q_h            = g_t_set[row, 'q_h']
+    sd_m_hat_prime = g_t_set[row, 'sd']
+    
+    x_matrix     <- matrix(c(rep(1, T_size), seq(1/T_size - i, 1-i, by = 1/T_size)), nrow=T_size, byrow=FALSE)
+    w_vector     <- sapply(seq(1/T_size - i, 1-i, by = 1/T_size)/h, FUN = epanechnikov_kernel)/h
+    XtW          <- t(apply(x_matrix,2,function(x){x*w_vector}))   #t(X) %*% diag(w)   computed faster.  :)
+    XtWX_inverse <- tryCatch({solve(XtW %*% x_matrix)},  
+                             error = function(e) {print("Something is wrong, the matrix can not be inverted")})
+    
+    m_hat_prime <- (XtWX_inverse %*% XtW %*% y_data_ar_1_with_trend)[2]
+    
+    if (m_hat_prime - q_h * sd_m_hat_prime > 0){
+      results_their = c(results_their, 1)
+    } else if (m_hat_prime + q_h * sd_m_hat_prime < 0) {
+      results_their = c(results_their, -1)
+    } else {
+      results_their = c(results_their, 0)
+    }
+    if (g_t_set[row, 'values_with_sign'] > g_t_set[row, 'lambda'] + gaussian_quantile){
+      results_our = c(results_our, 1)
+    } else if (-g_t_set[row, 'values_with_sign'] > g_t_set[row, 'lambda'] + gaussian_quantile){
+      results_our = c(results_our, -1)
+    } else {
+      results_our = c(results_our, 0)
+    }
+    
+    #cat("i = ", i, " and h = ", h, ": interval = [", m_hat_prime - q_h * sd_m_hat_prime, ",",m_hat_prime + q_h * sd_m_hat_prime, "]\n")
+  }
+  c(sum(abs(results_their)), sum(abs(results_our)))
 })
--- One_trend/Shape/simulations_based_on_data.R
+++ One_trend/Shape/simulations_based_on_data.R
@@ -40,7 +40,7 @@ simulations_based_on_data <- function(N, different_T, different_alpha, a_hat, si
       #Replicating test procedure N times
       size_of_the_test_ar_1 = replicate(N, {
         y_data_ar_1 = arima.sim(model = list(ar = a_hat), n = T, innov = rnorm(T, 0, sigma_eta))
-        sigmahat = estimating_sigma_for_AR1(y_data_ar_1, L1, L2)
+        sigmahat = estimating_sigma_for_AR1(y_data_ar_1, L1, L2)[[1]]
         result_notrend_ar1 = statistic_function(y_data_ar_1, g_t_set, kernel_ind, sigmahat)[[2]]
         if (result_notrend_ar1 > gaussian_quantile) {d = 1} else {d = 0}
         d
@@ -82,7 +82,7 @@ simulations_based_on_data <- function(N, different_T, different_alpha, a_hat, si
         size_of_the_test_with_trend = replicate(N, {
           #Adding a function that is 0 on the first half and linear on the second half
           y_data_ar_1_with_trend = m + arima.sim(model = list(ar = a_hat), innov = rnorm(T, 0, sigma_eta), n = T)
-          sigmahat = estimating_sigma_for_AR1(y_data_ar_1_with_trend, L1, L2)
+          sigmahat = estimating_sigma_for_AR1(y_data_ar_1_with_trend, L1, L2)[[1]]
           result_with_trend = statistic_function(y_data_ar_1_with_trend, g_t_set, kernel_ind, sigmahat)[[2]]
           if (result_with_trend > gaussian_quantile) {d = 1} else {d = 0}
           d
