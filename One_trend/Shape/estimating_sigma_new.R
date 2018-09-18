d1864615c50e201d70b3c5ca3ad202ece08a76fc
MKhismatullina <marina.k@uni-bonn.de>
2018-09-18 22:33:35 +0200

    Comparison of variance estmators

--- One_trend/Shape/estimating_sigma_new.R
+++ One_trend/Shape/estimating_sigma_new.R
@@ -66,7 +66,7 @@ estimating_sigma_new <- function(y_data, p){
   return(list(sigma_hat, a_hat, sigma_eta))
 }
 
-estimating_sigma_simple <- function(y_data, p){
+estimating_sigma_old_simple <- function(y_data, p){
   #Function that calculates the estimate of the square root of long-run variance sigma^2
   #Arguments:
   #y_data           vector of data length T 
@@ -93,11 +93,12 @@ estimating_sigma_simple <- function(y_data, p){
     gamma_hat <-  gamma_hat_temp[3:(p+2)]
     a_hat     <- inv(Gamma_matrix_hat) %*% gamma_hat
   }
-  sigma_eta_squared <- sum(a_hat * gamma_hat_temp[2:(p+1)]) - gamma_hat_temp[1]
-  sigma_eta <- sqrt(sigma_eta_squared)
-  sigma_hat <- sigma_eta / (abs(1 - sum(a_hat)))
-  return(list(sigma_hat, a_hat, sigma_eta))
-}
+#  sigma_eta_squared <- sum(a_hat * gamma_hat_temp[2:(p+1)]) - gamma_hat_temp[1]
+#  sigma_eta <- sqrt(sigma_eta_squared)
+#  sigma_hat <- sigma_eta / (abs(1 - sum(a_hat)))
+  return(a_hat)
+}  
+
 
 
 estimating_sigma_overidentified <- function(y_data, p){
@@ -119,11 +120,54 @@ estimating_sigma_overidentified <- function(y_data, p){
   Kappa_vector <- c(Kappa_vector, sum(gamma_hat[2:(p + 2)] * gamma_hat[(2 - i):(p + 2 - i)]))
   }
   a_hat     <- solve(Kappa_matrix_hat) %*% Kappa_vector
-  #sigma_eta_squared <- sum(a_hat * gamma_hat_temp[2:(p+1)]) - gamma_hat_temp[1]
-  #sigma_eta <- sqrt(sigma_eta_squared)
-  #sigma_hat <- sigma_eta / (abs(1 - sum(a_hat)))
   return(a_hat)
 }
+
+estimating_sigma_overidentified_twice <- function(y_data, p){
+  #Function that calculates the estimate of the square root of long-run variance sigma^2
+  #Arguments:
+  #y_data           vector of data length T 
+  #p                order of the AR(p) model
+  
+  y_data_diff <- c(0, diff(y_data))
+  
+  gamma_hat <- sapply(1:(p+3), FUN = sample_autocovariance, y_data_diff)
+  
+  Kappa_matrix_hat <- matrix(data = NA, nrow = p, ncol = p)
+  Kappa_vector     <- c()
+  for (i in 1:p){
+    for (j in 1:p){
+      Kappa_matrix_hat[i, j] = sum(gamma_hat[(2 - i):(p + 3 - i)] * gamma_hat[(2 - j):(p + 3 - j)])
+    }
+    Kappa_vector <- c(Kappa_vector, sum(gamma_hat[2:(p + 3)] * gamma_hat[(2 - i):(p + 3 - i)]))
+  }
+  a_hat     <- solve(Kappa_matrix_hat) %*% Kappa_vector
+  return(a_hat)
+}
+
+
+estimating_sigma_overidentified_3 <- function(y_data, p){
+  #Function that calculates the estimate of the square root of long-run variance sigma^2
+  #Arguments:
+  #y_data           vector of data length T 
+  #p                order of the AR(p) model
+  
+  y_data_diff <- c(0, diff(y_data))
+  
+  gamma_hat <- sapply(1:(p+4), FUN = sample_autocovariance, y_data_diff)
+  
+  Kappa_matrix_hat <- matrix(data = NA, nrow = p, ncol = p)
+  Kappa_vector     <- c()
+  for (i in 1:p){
+    for (j in 1:p){
+      Kappa_matrix_hat[i, j] = sum(gamma_hat[(2 - i):(p + 4 - i)] * gamma_hat[(2 - j):(p + 4 - j)])
+    }
+    Kappa_vector <- c(Kappa_vector, sum(gamma_hat[2:(p + 4)] * gamma_hat[(2 - i):(p + 4 - i)]))
+  }
+  a_hat     <- solve(Kappa_matrix_hat) %*% Kappa_vector
+  return(a_hat)
+}
+
 
 estimating_sigma_oracle <- function(y_data, p){
   #Function that calculates the estimate of the square root of long-run variance sigma^2
@@ -140,4 +184,3 @@ estimating_sigma_oracle <- function(y_data, p){
   a_hat     <- solve(t(X_matrix) %*%X_matrix) %*% t(X_matrix) %*% y_vector
   return(a_hat)
 }
-
--- One_trend/main_variance_simulation.R
+++ One_trend/main_variance_simulation.R
@@ -1,6 +1,6 @@
-#library(xtable)
-#options(xtable.floating = FALSE)
-#options(xtable.timestamp = "")
+library(xtable)
+options(xtable.floating = FALSE)
+options(xtable.timestamp = "")
 source("Shape/functions.R")
 source("Shape/estimating_sigma_new.R")
 
@@ -12,11 +12,12 @@ dyn.load("Shape/C_code/estimating_sigma.dll")
 #Defining necessary parameters#
 ###############################
 
-N                <- 1000 #Number of replications for comparison of the estimates
+#N                <- 1000 #Number of replications for comparison of the estimates
 different_T      <- c(250, 350, 500, 1000) #Different lengths of time series for which we compare the estimates
 different_a      <- c(-0.5, -0.25, 0.25, 0.5)
 different_slopes <- c(0)
 sigma_eta        <- 1
+vect <- c()
 
 for (a_1 in different_a){
   true_sigma <- sqrt(sigma_eta^2/((1 - a_1)^2))
@@ -27,15 +28,24 @@ for (a_1 in different_a){
     for (slope in different_slopes){
       for (i in 1:T_size) {line_trend[i] = (i - 0.5*T_size) * slope/T_size}
       y_data_ar_1_with_trend <- arima.sim(model = list(ar = a_1), n = T_size, innov = rnorm(T_size, 0, sigma_eta)) + line_trend
-#      var_old <- estimating_sigma_for_AR1(y_data_ar_1_with_trend, L1, L2)[[3]]
-      a_hat_overidentified <- estimating_sigma_overidentified(y_data_ar_1_with_trend, 1)
-      a_hat_oracle         <- estimating_sigma_oracle(y_data_ar_1_with_trend, 1)
+      a_hat_old                  <- estimating_sigma_old_simple(y_data_ar_1_with_trend, 1)
+      a_hat_overidentified       <- estimating_sigma_overidentified(y_data_ar_1_with_trend, 1)
+      a_hat_overidentified_twice <- estimating_sigma_overidentified_twice(y_data_ar_1_with_trend, 1)
+      a_hat_overidentified_3     <- estimating_sigma_overidentified_3(y_data_ar_1_with_trend, 1)
+      a_hat_oracle               <- estimating_sigma_oracle(y_data_ar_1_with_trend, 1)
       
 #      cat("Difference with old method: ", var_old, ", a_1 = ", a_1, ", slope = ", slope, ", T = ", T_size, "\n")
-      cat("Estimate of parameter a_1 with overidentified method: ", a_hat_overidentified, ", a_1 = ", a_1, ", slope = ", slope, ", T = ", T_size, "\n")
-      cat("Estimate of parameter a_1 with oracle method: ", a_hat_oracle, ", a_1 = ", a_1, ", slope = ", slope, ", T = ", T_size, "\n")
-      cat("\n")
+#      cat("Estimate of parameter a_1 with overidentified method: ", a_hat_overidentified, ", a_1 = ", a_1, ", slope = ", slope, ", T = ", T_size, "\n")
+#      cat("Estimate of parameter a_1 with twice overidentified method: ", a_hat_overidentified_twice, ", a_1 = ", a_1, ", slope = ", slope, ", T = ", T_size, "\n")
+      
+#      cat("Estimate of parameter a_1 with oracle method: ", a_hat_oracle, ", a_1 = ", a_1, ", slope = ", slope, ", T = ", T_size, "\n")
+#      cat("\n")
+      vect <- c(vect, T_size, a_1, a_hat_old, a_hat_overidentified, a_hat_overidentified_twice, a_hat_overidentified_3, a_hat_oracle)
     }
   }
 }
 
+matrix_ <- matrix(vect, nrow = length(different_T) * length(different_a), ncol = 7, byrow = TRUE)
+colnames(matrix_) <- c("T", "a_1", "simple", "overidentified", "overidentified*2", "overidentified*3", "oracle")
+
+print.xtable(xtable(matrix_, digits = c(3), align = "cccccccc"), type="latex",  file="Paper/Plots/variance_estimators.tex", include.colnames = TRUE, include.rownames =FALSE )
