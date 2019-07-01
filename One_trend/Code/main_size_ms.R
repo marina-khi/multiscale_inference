1f634f88e7132e51d6631b93b887a3e4cedd7c79
mkhismatullina <marina.k@uni-bonn.de>
2019-07-01 22:46:13 +0200

    Forgot to save the R script :)

--- One_trend/Code/main_size_ms.R
+++ One_trend/Code/main_size_ms.R
@@ -17,6 +17,9 @@ source("functions/calculating_size.r")
 source("functions/SiZer_functions.r")
 dyn.load("functions/SiZer_functions.dll")
 
+##################################################################
+#Calculating size for the estimated sigma for small parameters a1#
+##################################################################
 
 # Parameters
 different_T     <- c(250, 500, 1000)
@@ -29,7 +32,6 @@ SimRuns    <- 1000                  # number of simulation runs to produce criti
 kappa      <- 0.1                   # parameter to determine order statistic for the version
                                     # of the multiscale statistic from Section ?? 
 
-#Calculating size for the estimated sigma for small sample sizes
 number_of_cols            <- length(different_a1) * (length(different_alpha) + 1)
 size_matrix_ms            <- matrix(NA, nrow = length(different_T), ncol = number_of_cols)
 rownames(size_matrix_ms)  <- different_T
@@ -45,7 +47,10 @@ print.xtable(xtable(size_matrix_ms, digits = c(3), align = paste(replicate(numbe
   type="latex", file=paste0("plots/size_estimated_sigma.tex"), include.colnames = FALSE)
 
 
-#Calculating size for the true sigma for small sample sizes
+#############################################################
+#Calculating size for the true sigma for small parameters a1#
+#############################################################
+
 size_matrix_ms            <- matrix(NA, nrow = length(different_T), ncol = number_of_cols)
 rownames(size_matrix_ms)  <- different_T
 
@@ -88,7 +93,7 @@ rownames(size_ms_true_sigma) <- different_alpha
 
 k <- 1
 for (a1 in different_a1){
-  result <- calculating_size(a1, different_T, different_alpha, sigma_eta, Nsim = Nsim, kappa =kappa, SimRuns =SimRuns, type_of_sigma = 'true', q_ = 50, remove.small.ess = 'false')[[1]]
+  result <- calculating_size(a1, different_T, different_alpha, sigma_eta, Nsim = Nsim, kappa =0.1, SimRuns =SimRuns, type_of_sigma = 'true', q_ = 50, remove.small.ess = 'false')[[1]]
   size_ms_true_sigma[, (k * (length(different_T) + 1) - (length(different_T) - 1)):(k * (length(different_T) + 1))] <- t(result)
   k <- k + 1
 }
@@ -97,29 +102,28 @@ print.xtable(xtable(size_ms_true_sigma, digits = c(3), align = paste(replicate(n
              type="latex", file=paste0("plots/size_persistent_true_sigma.tex"), include.colnames = FALSE)
 
 
+#########################
+#Calculating global size#
+#########################
 
-# Parameters
-different_T     <- c(1000)
+different_T     <- c(250, 500, 1000)
 different_a1    <- c(-0.5, 0.5)
 different_alpha <- c(0.05)
 
 Nsim       <- 1000                  # number of simulation runs for size/power calculations
 sigma_eta  <- 1                     # standard deviation of the innovation term in the AR model
 SimRuns    <- 1000                  # number of simulation runs to produce critical values
-kappa      <- 0.1                   # parameter to determine order statistic for the version
-# of the multiscale statistic from Section ?? 
 
-#Calculating size for the true sigma and for different versions of the test
 number_of_cols         <- length(different_a1) * 5
 size_matrix            <- matrix(NA, nrow = length(different_T), ncol = number_of_cols)
 rownames(size_matrix)  <- different_T
 
 k <- 1
 for (a1 in different_a1){
-  # size <- calculating_size(a1, different_T, different_alpha, sigma_eta, Nsim = Nsim, kappa =kappa, SimRuns =SimRuns, type_of_sigma = 'true', q_ = 25, remove.small.ess = 'true')
-  # size_matrix[, (k-1)*5 + 2] <- size[[1]]
-  # size_matrix[, (k-1)*5 + 3] <- size[[2]]
-  # size_matrix[, (k-1)*5 + 4] <- size[[3]]
+  size <- calculating_size(a1, different_T, different_alpha, sigma_eta, Nsim = Nsim, kappa =0.1, SimRuns =SimRuns, type_of_sigma = 'true', q_ = 25, remove.small.ess = 'true')
+  size_matrix[, (k-1)*5 + 2] <- size[[1]]
+  size_matrix[, (k-1)*5 + 3] <- size[[2]]
+  size_matrix[, (k-1)*5 + 4] <- size[[3]]
   size.SiZer <- calculating_size_for_SiZer(a1, different_T, different_alpha, sigma_eta, Nsim = 1000, SimRuns =1000)
   size_matrix[, (k-1)*5 + 5] <- size.SiZer
   k <- k + 1
@@ -128,9 +132,35 @@ for (a1 in different_a1){
 print.xtable(xtable(size_matrix, digits = c(3), align = paste(replicate(number_of_cols + 1, "c"), collapse = "")),
              type="latex", file=paste0("plots/comparing_size.tex"), include.colnames = FALSE)
 
+#################################################################
+#Calculating rowwise size and creating parallel coordinate plots#
+#################################################################
 
+T            <- 500
+different_a1 <- c(-0.5, 0.5)
+alpha        <- 0.05
 
+Nsim       <- 1000                  # number of simulation runs for size/power calculations
+sigma_eta  <- 1                     # standard deviation of the innovation term in the AR model
+SimRuns    <- 1000                  # number of simulation runs to produce critical values
 
+for (a1 in different_a1){
+  result <- calculating_size_rowwise(a1, T, alpha, sigma_eta, Nsim = Nsim, kappa =0.1, SimRuns =SimRuns)
+  h.grid <- result$h.grid
+  
+  pdffilename <- paste0("plots/pcp_rowwise_T_", T, "_a1_", a1*100, ".pdf")
+  pdf(pdffilename, height = 7, width = 10, paper = 'special')
 
-
-
+  par(mfrow = c(1,1), cex = 1.0, tck = -0.025) #Setting the layout of the graphs
+  par(mar = c(0, 0.5, 0, 0)) #Margins for each plot
+  par(oma = c(2.5, 1.5, 0.2, 0.2)) #Outer margins
+  
+  plot(h.grid, result$size_ms*100, ylim = c(0, 15), xlab = 'bandwidth',
+       ylab = "Rowwise % sig.", type = 'l')
+  points(h.grid, result$size_uncor*100, type = 'l', lty = 2)
+  points(h.grid, result$size_rows*100, type = 'l', lty = 3)
+  points(h.grid, result$size_SiZer*100, type = 'l', lty = 4)
+  
+  legend(0.03, 15, legend=c("MS method", "Uncorrected version", "Rowwise version", "SiZer method"), lty=1:4, cex=1.1)
+  dev.off()
+}
