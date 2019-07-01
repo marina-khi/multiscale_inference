35f34b21a0af7c24d0ea25789b639b51661cfa88
mkhismatullina <marina.k@uni-bonn.de>
2019-07-01 22:44:23 +0200

    Rowwise size comparison

--- One_trend/Code/functions/calculating_size.R
+++ One_trend/Code/functions/calculating_size.R
@@ -84,9 +84,7 @@ calculating_size <- function(a1, different_T, different_alpha, sigma_eta, Nsim =
 
 calculating_size_for_SiZer <- function(a1, different_T, alpha, sigma_eta, Nsim = 1000, SimRuns =1000){
   size    <- c()
-
   for (T in different_T){
-    
     # Construct grid
     grid      <- grid_construction(T)
     gset      <- grid$gset
@@ -124,4 +122,76 @@ calculating_size_for_SiZer <- function(a1, different_T, alpha, sigma_eta, Nsim =
     rm(size_temp)
   }
   return(size.results.sizer = size)
+}
+
+calculating_size_rowwise <- function(a1, T, alpha, sigma_eta, Nsim = 1000, kappa =0.1, SimRuns =1000){
+  
+  # Construct grid
+  grid      <- grid_construction(T)
+  gset      <- grid$gset
+  u.grid    <- sort(unique(gset[,1]))
+  h.grid    <- sort(unique(gset[,2]))
+    
+  autocov   <- (sigma_eta^2/(1-a1^2)) * (a1^seq(0,T-1,by=1))
+  ess       <- ESS.star(u.grid=u.grid, h.grid=h.grid, T=T, autocov=autocov)
+  deletions <- ess$del
+  grid      <- grid_construction(T=T, u.grid=u.grid, h.grid=h.grid, deletions=deletions)
+  
+  h.grid.new <- sort(unique(grid$gset[,2]))
+  
+  size_ms_temp     <- matrix(NA, nrow = Nsim, ncol = length(h.grid.new))
+  size_uncor_temp  <- matrix(NA, nrow = Nsim, ncol = length(h.grid.new))
+  size_rows_temp   <- matrix(NA, nrow = Nsim, ncol = length(h.grid.new))
+  size_SiZer_temp  <- matrix(NA, nrow = Nsim, ncol = length(h.grid.new))
+
+  # Compute kernel weights and critical value for multiscale test
+  wghts  <- kernel_weights(T=T, grid=grid)
+  quants <- multiscale_quantiles(T=T, grid=grid, weights=wghts, kappa=kappa, SimRuns=SimRuns)
+
+  sizer.wghts  <- SiZer_weights(T=T, grid=grid)
+  sizer.std    <- SiZer_std(weights=sizer.wghts, autocov=autocov, T)
+  sizer.quants <- SiZer_quantiles(alpha=alpha, T=T, grid=grid, autocov=autocov)
+  
+  set.seed(1)
+  for (i in 1:Nsim){
+    #Simulating the time series
+    data.simulated <- simulating_data(T, a1, sigma_eta, sim.design = 'constant')
+    data           <- data.simulated$data
+    trend          <- data.simulated$trend
+    sigma_true     <- data.simulated$sigma
+      
+    stats    <- multiscale_statistics(data=data, weights=wghts, sigmahat=sigma_true, grid=grid)
+    vals     <- stats$values
+      
+    test.res <- multiscale_testing(alpha=alpha, quantiles=quants, values=vals, grid=grid)
+        
+    test.res$test_ms[test.res$test_ms == 2] <- NA
+    test.res$test_uncor[test.res$test_uncor == 2] <- NA
+    test.res$test_rows[test.res$test_rows == 2] <- NA        
+
+    
+    sizer.values  <- sizer.wghts %*% data
+    sizer.vals    <- as.vector(sizer.values)
+    
+    SiZer_results <- SiZer_test(values=sizer.vals, std.devs=sizer.std, quants=sizer.quants, grid=grid)
+    
+    SiZer_results$test[SiZer_results$test == 2] <- NA
+    
+    
+    for (j in 1:length(h.grid.new)){
+      bw  <- h.grid.new[j]
+      h_index <- match(bw, h.grid)
+        
+      if (sum(abs(test.res$test_ms[h_index, ]), na.rm = TRUE) >0) {size_ms_temp[i, j] <- 1} else {size_ms_temp[i, j] <- 0}
+      if (sum(abs(test.res$test_uncor[h_index, ]), na.rm = TRUE) >0) {size_uncor_temp[i, j] <- 1} else {size_uncor_temp[i, j] <- 0}
+      if (sum(abs(test.res$test_rows[h_index, ]), na.rm = TRUE) >0) {size_rows_temp[i, j] <- 1} else {size_rows_temp[i, j] <- 0}
+      if (sum(abs(SiZer_results$test[h_index, ]), na.rm = TRUE) >0) {size_SiZer_temp[i, j] <- 1} else {size_SiZer_temp[i, j] <- 0}
+    }
+  }
+  size_ms    <- colSums(size_ms_temp)/Nsim
+  size_uncor <- colSums(size_uncor_temp)/Nsim
+  size_rows  <- colSums(size_rows_temp)/Nsim
+  size_SiZer <- colSums(size_SiZer_temp)/Nsim
+
+  return(list(size_ms = size_ms, size_uncor = size_uncor, size_rows = size_rows, size_SiZer = size_SiZer, h.grid = h.grid.new))
 }
