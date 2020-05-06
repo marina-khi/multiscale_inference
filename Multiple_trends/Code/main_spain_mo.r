78f9c2f80d2f0a3759c095a804d3b56c06a8ba4f
MKhismatullina <marina.k@uni-bonn.de>
2020-05-06 20:41:44 +0200

    Multiscale package is up and running!

--- Multiple_trends/Code/main_spain_mo.r
+++ Multiple_trends/Code/main_spain_mo.r
@@ -5,35 +5,32 @@ rm(list=ls())
 
 library(Rcpp)
 library(tidyr)
-library(tictoc)
 
 # The following file contains functions that are used to estimate the AR parameters, in particular, to compute
 # the estimators of the coefficients, a_1, ... ,a_p, the estimator of the variance of the innovation term, sigma_eta^2 
 # and the estimator of the long-run variance sigma^2.
-source("functions/long_run_variance.r")
-
-#Load necessary functions  
-source("functions/ConstructGrid.r")
-source("functions/multiscale_statistics.r")
-source("functions/multiscale_quantiles.r")
-source("functions/multiscale_testing.r")
-source("functions/minimal_intervals.r")
-source("functions/functions.r")
-sourceCpp("functions/multiscale_statistics.cpp")
+source("functions/estimate_lrv.R")
+source("functions/construct_grid.R")
+source("functions/plot_SiZer_map.R")
+source("functions/compute_quantiles.R")
+source("functions/multiscale_test.R")
+source("functions/compute_minimal_intervals.R")
+source("functions/functions.R")
+sourceCpp("functions/multiscale_stats.cpp")
 
 
 alpha   <- 0.05 # alpha for calculating quantiles
-SimRuns <- 5000 # number of simulation runs to produce critical values
+SimRuns <- 100 # number of simulation runs to produce critical values
 
-#Loading the real data for yearly temperature in England
+#Loading the real data for mortality in Spain
 spain_mo <- read.csv("data/mortality_spain.csv", sep = ",", dec = ".", stringsAsFactors = FALSE, na.strings = "N/A")
 spain_mo <- spain_mo[spain_mo$ambito == 'nacional' & spain_mo$cod_sexo == 'all' & spain_mo$cod_gedad =='all', 'defunciones_observadas']
 
 Tlen      <- length(spain_mo)
 
 #Order selection
-source("functions/order_selection.r")
-orders <- order_selection(as.matrix(spain_mo[1:700]), q = 30:50, r = 10:15)
+source("functions/select_order.R")
+orders <- select_order(as.matrix(spain_mo[1:700]), q = 30:50, r = 10:15)
 
 
 #Setting tuning parameters for testing
@@ -46,21 +43,22 @@ ts_end   = ts_start + Tlen - 1 #the last point of time series
 
 #Estimate the long-run variance
 sigmahat_vector_order_9 <- c()
-AR.struc  <- AR_lrv(data=spain_mo, q = q, r.bar = r, p = p)
+AR.struc  <- AR_lrv(data=spain_mo[1:690], q = q, r.bar = r, p = p)
 sigma_hat <- sqrt(AR.struc$lrv)
 sigmahat_vector_order_9 <- c(sigmahat_vector_order_9, sigma_hat)
 
 set.seed(1)
 #Compute test results for the multiscale method
-results <- multiscale_testing(alpha = alpha, data = spain_mo, sigma_vec = sigmahat_vector_order_9, SimRuns = SimRuns, N_ts = 1)
+results <- multiscale_test(alpha = alpha, data = spain_mo, sigma_vec = sigmahat_vector_order_9, SimRuns = SimRuns, N_ts = 1, deriv_order = 1)
 quant   <- results$quant
+
 
 #Produce minimal intervals (Here - only the increases! But this is because we do not have decreases for our applications)
 a_t_set <- subset(results$gset, test == 1, select = c(u, h, vals))
 p_t_set <- data.frame('startpoint' = (a_t_set$u - a_t_set$h)*Tlen, 'endpoint' = (a_t_set$u + a_t_set$h)*Tlen, 'values' = a_t_set$vals)
-#p_t_set$endpoint[p_t_set$endpoint > ts_end] <- ts_end
+p_t_set$endpoint[p_t_set$endpoint > ts_end] <- ts_end
 #p_t_set <- subset(p_t_set, endpoint <= ts_end, select = c(startpoint, endpoint, values)) 
-p_t_set <- choosing_minimal_intervals(p_t_set)
+p_t_set <- compute_minimal_intervals(p_t_set)
 
 
 pdf("plots/spain_mo.pdf", width=7, height=6.6, paper="special")
@@ -90,7 +88,7 @@ segments(p_t_set[['startpoint']], p_t_set$plottingindex, p_t_set$endpoint, p_t_s
 
 #SiZer 
 par(mar = c(0.5, 0.5, 2, 0)) #Margins for each plot
-SiZermap(sort(unique(results$gset[,1])), sort(unique(results$gset[,2])), results$test_matrix, plot.title = expression((c) ~ SiZer ~ map ~ 'for' ~ italic(T)[MS]))
+plot_SiZer_map(sort(unique(results$gset[,1])), sort(unique(results$gset[,2])), results$test_matrix, plot.title = expression((c) ~ SiZer ~ map ~ 'for' ~ italic(T)[MS]))
 
 axis_at = seq(4/Tlen, 724/Tlen, by = 30/Tlen)
 axis_labels = seq(as.Date("2018/4/4"), as.Date("2020/4/4"), by = 30)
