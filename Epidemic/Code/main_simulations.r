3b6397ca799af86d54091fc5cc7e9726fcddc2af
mkhismatullina <marina.k@uni-bonn.de>
2020-07-08 18:15:59 +0200

    Literature on econometrics and cleaned up supplememnt

--- Epidemic/Code/main_simulations.r
+++ Epidemic/Code/main_simulations.r
@@ -16,43 +16,41 @@ number_of_cols <- length(n_ts_vec) * length(alpha_vec) #Needed for the output
 #Auxiliary functions
 source("functions/functions.r")
 
-  for (sigma in sigma_vec){
-    number_of_cols         <- length(n_ts_vec) * length(alpha_vec)
-    size_matrix            <- matrix(NA, nrow = length(t_len_vec), ncol = number_of_cols)
-    rownames(size_matrix)  <- paste0("$T = ", t_len_vec, "$")
+for (sigma in sigma_vec){
+  number_of_cols         <- length(n_ts_vec) * length(alpha_vec)
+  size_matrix            <- matrix(NA, nrow = length(t_len_vec), ncol = number_of_cols)
+  rownames(size_matrix)  <- paste0("$T = ", t_len_vec, "$")
 
-    k <- 1
-    for (n_ts in n_ts_vec){
-      i <- 1
-      for (t_len in t_len_vec){
-        lambda_vec <- lambda_fct((1:t_len) / t_len, c = 1000)
-        pdf(paste0("plots/lambda_fct.pdf"), width=5, height=5, paper="special")
-        par(mar = c(3, 2, 2, 0)) #Margins for each plot
-        par(oma = c(0.2, 0.2, 0.2, 0.2)) #Outer margins
-        plot((1:t_len) / t_len, lambda_vec,  ylim = c(0, max(lambda_vec) + 100), xlab="u", ylab = "", mgp=c(2,0.5,0), type = "l")
-        title(main = expression(Plot ~ of ~ the ~ "function" ~ lambda), line = 1)
-        dev.off()
+  k <- 1
+  for (n_ts in n_ts_vec){
+    i <- 1
+    for (t_len in t_len_vec){
+      lambda_vec <- lambda_fct((1:t_len) / t_len, c = 1000)
+      pdf(paste0("plots/lambda_fct.pdf"), width=5, height=5, paper="special")
+      par(mar = c(3, 2, 2, 0)) #Margins for each plot
+      par(oma = c(0.2, 0.2, 0.2, 0.2)) #Outer margins
+      plot((1:t_len) / t_len, lambda_vec,  ylim = c(0, max(lambda_vec) + 100), xlab="u", ylab = "", mgp=c(2,0.5,0), type = "l")
+      title(main = expression(Plot ~ of ~ the ~ "function" ~ lambda), line = 1)
+      dev.off()
 
-        set.seed(1) # This is for calculating size for different specifications on comparable datasets
-        size <- calculate_size(t_len = t_len, n_ts = n_ts, alpha_vec = alpha_vec,
-                               lambda_vec = lambda_vec, sigma = sigma,
-                               n_sim = n_sim, sim_runs = sim_runs, iid = FALSE)
-        size_matrix[i, (k * length(alpha_vec) - (length(alpha_vec) - 1)):(k * length(alpha_vec))] <- size
-        i <- i + 1
-      }
-      k <- k + 1
+      set.seed(10) # This is for calculating size for different specifications on comparable datasets
+      size <- calculate_size(t_len = t_len, n_ts = n_ts, alpha_vec = alpha_vec,
+                             lambda_vec = lambda_vec, sigma = sigma,
+                             n_sim = n_sim, sim_runs = sim_runs, iid = FALSE)
+      size_matrix[i, (k * length(alpha_vec) - (length(alpha_vec) - 1)):(k * length(alpha_vec))] <- size
+      i <- i + 1
     }
-
-    print.xtable(xtable(size_matrix, digits = c(3), align = paste(replicate(number_of_cols + 1, "c"), collapse = "")),
-                 type="latex", file=paste0("plots/size_overdispersion_", sigma, ".tex"),
-                 include.colnames = FALSE, sanitize.text.function = function(x) {x})
+    k <- k + 1
   }
+  print.xtable(xtable(size_matrix, digits = c(3), align = paste(replicate(number_of_cols + 1, "c"), collapse = "")),
+               type="latex", file=paste0("plots/size_overdispersion_", sigma, ".tex"),
+               include.colnames = FALSE, sanitize.text.function = function(x) {x})
+}
 
 
 for (sigma in sigma_vec){
   power_matrix           <- matrix(NA, nrow = length(t_len_vec), ncol = number_of_cols)
   rownames(power_matrix) <- paste0("$T = ", t_len_vec, "$")
-
   k <- 1
   for (n_ts in n_ts_vec){
     i <- 1
