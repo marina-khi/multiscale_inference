a6dd3a1049e80cd1b1ff5b4305328b385d8c7b68
mkhismatullina <marina.k@uni-bonn.de>
2020-07-20 23:50:27 +0200

    Modified the simulation setting a bit

--- Epidemic/Code/main_simulations.r
+++ Epidemic/Code/main_simulations.r
@@ -19,10 +19,10 @@ seed <- sample(1:100000, 1)
 #Calculating size#
 ##################
 
-n_sim     <- 5000               # number of simulation runs for power and size
+n_sim     <- 10               # number of simulation runs for power and size
 sim_runs  <- 5000               # number of simulation runs to produce critical values
 alpha_vec <- c(0.01, 0.05, 0.1) # different significance levels
-n_ts_vec  <- c(5, 10, 50)       # different number of time series
+n_ts_vec  <- c(5, 10)       # different number of time series
 t_len_vec <- c(100, 250, 500)   # different time series lengths
 sigma_vec <- c(15, 20, 10)      # different overdispersion parameter
 
@@ -48,27 +48,27 @@ dev.off()
 #with the values that your preferred function takes at time points 1/t_len, 2/t_len, ..., 1.
 
 
-# for (sigma in sigma_vec){
-#   size_matrix            <- matrix(NA, nrow = length(t_len_vec), ncol = number_of_cols)
-#   rownames(size_matrix)  <- paste0("$T = ", t_len_vec, "$")
-# 
-#   k <- 1
-#   for (n_ts in n_ts_vec){
-#     i <- 1
-#     for (t_len in t_len_vec){
-#       set.seed(321)
-#       size <- calculate_size(t_len = t_len, n_ts = n_ts, alpha_vec = alpha_vec,
-#                              lambda_vec = lambda_vec, sigma = sigma,
-#                              n_sim = n_sim, sim_runs = sim_runs)
-#       size_matrix[i, (k * length(alpha_vec) - (length(alpha_vec) - 1)):(k * length(alpha_vec))] <- size
-#       i <- i + 1
-#     }
-#     k <- k + 1
-#   }
-#   print.xtable(xtable(size_matrix, digits = c(3), align = paste(replicate(number_of_cols + 1, "c"), collapse = "")),
-#                type="latex", file=paste0("plots/size_overdispersion_", sigma, ".tex"),
-#                include.colnames = FALSE, sanitize.text.function = function(x) {x})
-# }
+for (sigma in sigma_vec){
+  size_matrix            <- matrix(NA, nrow = length(t_len_vec), ncol = number_of_cols)
+  rownames(size_matrix)  <- paste0("$T = ", t_len_vec, "$")
+
+  k <- 1
+  for (n_ts in n_ts_vec){
+    i <- 1
+    for (t_len in t_len_vec){
+      set.seed(321)
+      size <- calculate_size(t_len = t_len, n_ts = n_ts, alpha_vec = alpha_vec,
+                             lambda_vec = lambda_vec, sigma = sigma,
+                             n_sim = n_sim, sim_runs = sim_runs)
+      size_matrix[i, (k * length(alpha_vec) - (length(alpha_vec) - 1)):(k * length(alpha_vec))] <- size
+      i <- i + 1
+    }
+    k <- k + 1
+  }
+  # print.xtable(xtable(size_matrix, digits = c(3), align = paste(replicate(number_of_cols + 1, "c"), collapse = "")),
+  #              type="latex", file=paste0("plots/size_overdispersion_", sigma, ".tex"),
+  #              include.colnames = FALSE, sanitize.text.function = function(x) {x})
+}
 
 #Now the results of size simulations are stored as the tex tables in the folder ./plots/
 
@@ -77,10 +77,10 @@ dev.off()
 #Calculating power for mean functions with different heights#
 #############################################################
 
-n_sim     <- 5000               # number of simulation runs for power and size
+n_sim     <- 10               # number of simulation runs for power and size
 sim_runs  <- 5000               # number of simulation runs to produce critical values
 alpha_vec <- c(0.01, 0.05, 0.1) # different significance levels
-n_ts_vec  <- c(5, 10, 50)       # different number of time series
+n_ts_vec  <- c(5, 10)       # different number of time series
 t_len_vec <- c(100, 250, 500)   # different time series lengths
 sigma_vec <- c(15, 20, 10)      # different overdispersion parameter
 
@@ -88,7 +88,7 @@ number_of_cols <- length(n_ts_vec) * length(alpha_vec) #Needed for the output
 
 #As the mean functions in these power simulations, we take the following functions:
 #lambda_1(u) = 6000 * exp(-(10 * u - 3) ^ 2 / 2) + 1000 for 0 <= u <= 1.
-#lambda_2(u) = 5000 * exp(-(10 * u - 3) ^ 2 / 2) + 1000 for 0 <= u <= 1.
+#lambda(u) = 5000 * exp(-(10 * u - 3) ^ 2 / 2) + 1000 for 0 <= u <= 1.
 
 #Here are the plots of these functions:
 
@@ -102,9 +102,9 @@ par(mgp = c(3, 0.5, 0))
 plot((1:100) / 100, lambda_vec_1,  ylim = c(0, max(lambda_vec_1, lambda_vec_2) + 100),
      xlab="", ylab = "", type = "l")
 lines((1:100) / 100, lambda_vec_2, type = "l", col = "red")
-title(main = expression(Plot ~ of ~ the ~ "functions" ~ lambda[1] ~ and ~ lambda[2]), line = 1)
+title(main = expression(Plot ~ of ~ the ~ "functions" ~ lambda[1] ~ and ~ lambda), line = 1)
 title(xlab="u", line=1.5)
-legend("topright", inset = 0.02, legend=c(expression(lambda[1](u) ~" "), expression(lambda[2](u) ~" ")),
+legend("topright", inset = 0.02, legend=c(expression(lambda[1](u) ~" "), expression(lambda(u) ~" ")),
        col = c("black", "red"), lty = 1, cex = 0.95, ncol = 1)
 dev.off()
 
@@ -135,10 +135,10 @@ for (sigma in sigma_vec){
     k <- k + 1
   }
     
-  print.xtable(xtable(power_matrix, digits = c(3),
-                      align = paste(replicate(number_of_cols + 1, "c"), collapse = "")),
-               type="latex", file=paste0("plots/power_sigma_", sigma, "_higher_peak.tex"),
-               include.colnames = FALSE, sanitize.text.function = function(x) {x})
+  # print.xtable(xtable(power_matrix, digits = c(3),
+  #                     align = paste(replicate(number_of_cols + 1, "c"), collapse = "")),
+  #              type="latex", file=paste0("plots/power_sigma_", sigma, "_higher_peak.tex"),
+  #              include.colnames = FALSE, sanitize.text.function = function(x) {x})
 }
 
 #Now the results of power simulations for this scenario are stored as the tex tables in the folder ./plots/
@@ -148,10 +148,10 @@ for (sigma in sigma_vec){
 #Calculating power for mean functions with different peak locations#
 ####################################################################
 
-n_sim     <- 5000               # number of simulation runs for power and size
+n_sim     <- 10               # number of simulation runs for power and size
 sim_runs  <- 5000               # number of simulation runs to produce critical values
 alpha_vec <- c(0.01, 0.05, 0.1) # different significance levels
-n_ts_vec  <- c(5, 10, 50)       # different number of time series
+n_ts_vec  <- c(5, 10)       # different number of time series
 t_len_vec <- c(100, 250, 500)   # different time series lengths
 sigma_vec <- c(15, 20, 10)      # different overdispersion parameter
 
@@ -159,7 +159,7 @@ number_of_cols <- length(n_ts_vec) * length(alpha_vec) #Needed for the output
 
 #As the mean function in these power simulations, we take the following functions:
 #lambda_1(u) = 5000 * exp(-(9 * u - 3) ^ 2 / 2) + 1000 for 0 <= u <= 1.
-#lambda_2(u) = 5000 * exp(-(10 * u - 3) ^ 2 / 2) + 1000 for 0 <= u <= 1.
+#lambda(u) = 5000 * exp(-(10 * u - 3) ^ 2 / 2) + 1000 for 0 <= u <= 1.
 
 #Here are the plots of these functions:
 
@@ -173,9 +173,9 @@ par(mgp = c(3, 0.5, 0))
 plot((1:100) / 100, lambda_vec_1,  ylim = c(0, max(lambda_vec_1, lambda_vec_2) + 100),
      xlab="", ylab = "", type = "l")
 lines((1:100) / 100, lambda_vec_2, type = "l", col = "red")
-title(main = expression(Plot ~ of ~ the ~ "functions" ~ lambda[1] ~ and ~ lambda[2]), line = 1)
+title(main = expression(Plot ~ of ~ the ~ "functions" ~ lambda[1] ~ and ~ lambda), line = 1)
 title(xlab="u", line=1.5)
-legend("topright", inset = 0.02, legend=c(expression(lambda[1](u) ~" "), expression(lambda[2](u) ~" ")),
+legend("topright", inset = 0.02, legend=c(expression(lambda[1](u) ~" "), expression(lambda(u) ~" ")),
        col = c("black", "red"), lty = 1, cex = 0.95, ncol = 1)
 dev.off()
 
@@ -207,10 +207,10 @@ for (sigma in sigma_vec){
     }
     k <- k + 1
   }
-  print.xtable(xtable(power_matrix, digits = c(3),
-                      align = paste(replicate(number_of_cols + 1, "c"), collapse = "")),
-               type="latex", file=paste0("plots/power_sigma_", sigma, "_shifted_peak.tex"),
-               include.colnames = FALSE, sanitize.text.function = function(x) {x})
+  # print.xtable(xtable(power_matrix, digits = c(3),
+  #                     align = paste(replicate(number_of_cols + 1, "c"), collapse = "")),
+  #              type="latex", file=paste0("plots/power_sigma_", sigma, "_shifted_peak.tex"),
+  #              include.colnames = FALSE, sanitize.text.function = function(x) {x})
 }
 
 #Now the results of power simulations for this scenario are stored as the tex tables in the folder ./plots/
