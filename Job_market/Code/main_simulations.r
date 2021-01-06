f0e90e2b45a88a120505e0bd967b729c99fe0872
mkhismatullina <marina.k@uni-bonn.de>
2021-01-06 13:07:52 +0100

    Presentation

--- Job_market/Code/main_simulations.r
+++ Job_market/Code/main_simulations.r
@@ -23,7 +23,7 @@ n_sim     <- 5000               # number of simulation runs for power and size
 sim_runs  <- 5000               # number of simulation runs to produce critical values
 alpha_vec <- c(0.05) # different significance levels
 n_ts_vec  <- c(5)       # different number of time series
-t_len_vec <- c(100, 250, 500, 1000, 2000, 5000)   # different time series lengths
+t_len_vec <- c(100, 250, 500)   # different time series lengths
 sigma_vec <- c(15)      # different overdispersion parameter
 
 number_of_cols <- length(n_ts_vec) * length(alpha_vec) #Needed for the output
@@ -77,28 +77,18 @@ for (sigma in sigma_vec){
     k <- k + 1
   }
   
-  pdf("plots_new/size_with_correction.pdf", width = 5, height = 6.5, paper="special")
-  layout(matrix(c(1, 2), ncol=1), widths=c(2.4, 2.4),
-         heights=c(1.5, 1.8), TRUE)
-  
+  pdf("plots_new/size_with_correction.pdf", width = 5.5, height = 3, paper="special")
+
   #Setting the layout of the graphs
   par(cex = 1, tck = -0.025)
-  par(mar = c(0.5, 0.5, 2, 0)) #Margins for each plot
-  par(oma = c(0.2, 1.5, 0.2, 0.2)) #Outer margins
+  par(mar = c(3, 3, 0.5, 0)) #Margins for each plot
+  par(oma = c(0.2, 0.2, 0.2, 0.2)) #Outer margins
   
-  plot(size_matrix[, 1], ylim=c(0, 0.1), type="l",
-       col = "#EB811B", ylab="", xlab="", mgp=c(1, 0.5, 0))
-  lines(size_matrix_2[, 1], col="#604c38")
+
+  plot(t_len_vec, size_matrix[, 1], ylim=c(0, 0.1), type="l",
+       col = "#EB811B", ylab="Empriical size", xlab="Sample size", mgp=c(1, 0.5, 0))
+  lines(t_len_vec, size_matrix_2[, 1], col="#604c38")
   title(main = "(a) size comparison", font.main = 1, line = 0.5)
-  legend("topright", inset = 0.02, legend=c("with correction", "without correction"),
-         col = c("#EB811B", "#604c38"), lty = 1, cex = 0.95, ncol = 1)
-  
-  par(mar = c(2.7, 0.5, 3, 0)) #Margins for each plot
-  
-  plot(size_matrix[, 1], ylim=c(0, 0.1), type="l",
-       col = "#EB811B", ylab="", xlab="", mgp=c(1, 0.5, 0))
-  lines(size_matrix_2[, 1], col="#604c38")
-  title(main = "(b) size comparison", font.main = 1, line = 0.5)
   legend("topright", inset = 0.02, legend=c("with correction", "without correction"),
          col = c("#EB811B", "#604c38"), lty = 1, cex = 0.95, ncol = 1)
   
