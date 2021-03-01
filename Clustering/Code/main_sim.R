69f710c5d3fa6700dce5bcda78efe1c3db197a3c
= <=>
2021-03-01 13:26:58 +0100

    First simulation draft

--- Clustering/Code/main_sim.R
+++ Clustering/Code/main_sim.R
@@ -15,11 +15,11 @@ library(dendextend)
 source("functions.R")
 
 #Defining necessary constants
-b_bar  <- 2
+b_bar  <- 1.6
 bw_abs <- 7
 t_len  <- 200
 n_ts   <- 6
-sigma  <- 15
+sigma  <- 10
 
 # functions for data simulations
 lambda_fct <- function(u, c = 1000, height = 5000, position = 10) {
@@ -38,25 +38,51 @@ simulate_data <- function(n_ts, t_len, lambda_vec, sigma) {
   return(data)
 }
 
-
-lambda_vec_1 <- lambda_fct((1:t_len) / t_len, c = 1000, height = 6000, position = 10)
-lambda_vec   <- lambda_fct((1:t_len) / t_len, c = 1000, height = 5000, position = 10)
-
-par(mar = c(3, 2, 2, 0)) #Margins for each plot
-par(oma = c(0.2, 0.2, 0.2, 0.2)) #Outer margins
-plot((1:t_len) / t_len, lambda_vec,  ylim = c(0, max(lambda_vec) + 100), xlab="u",
-     ylab = "", mgp=c(2,0.5,0), type = "l")
-title(main = expression(Plot ~ of ~ the ~ "function" ~ lambda), line = 1)
+lambda_vec_1 <- lambda_fct((1:t_len) / t_len, c = 1000, height = 5000, position = 10)
+lambda_vec_2 <- lambda_fct((1:t_len) / t_len, c = 1000, height = 5000, position = 11)
 
 Y1 <- simulate_data(n_ts = 1, t_len = t_len, lambda_vec = lambda_vec_1, sigma = sigma)
-Y2 <- simulate_data(n_ts = n_ts - 1, t_len = t_len, lambda_vec = lambda_vec_2, sigma = sigma)
-Y  <- cbind(Y1, Y2)
+Y2 <- simulate_data(n_ts = 1, t_len = t_len, lambda_vec = lambda_vec_2, sigma = sigma)
+#Y2 <- simulate_data(n_ts = n_ts - 1, t_len = t_len, lambda_vec = lambda_vec, sigma = sigma)
+#Y  <- cbind(Y1, Y2)
 
+plot((1:t_len) / t_len, Y1,  ylim = c(0, max(Y1, Y2) + 100), xlab="u",
+     ylab = "", mgp=c(2,0.5,0), type = "l")
+lines((1:t_len) / t_len, lambda_vec_1,  lty = "dashed")
+lines((1:t_len) / t_len, Y2, type = "l", col = "blue")
+lines((1:t_len) / t_len, lambda_vec_2, col = "blue", lty = "dashed")
+title(main = expression(Plot ~ of ~ the ~ time ~ series ~ 1  ~ and  ~ 2), line = 1)
 
+lambda_vec_3 <- (1:t_len) * 20 + 1000
+lambda_vec_4 <- lambda_vec_3 * 1.3
+
+Y3 <- simulate_data(n_ts = 1, t_len = t_len, lambda_vec = lambda_vec_3, sigma = sigma)
+Y4 <- simulate_data(n_ts = 1, t_len = t_len, lambda_vec = lambda_vec_4, sigma = sigma)
+
+plot((1:t_len) / t_len, Y3,  ylim = c(0, max(Y3, Y4) + 100), xlab="u",
+     ylab = "", mgp=c(2,0.5,0), type = "l")
+lines((1:t_len) / t_len, lambda_vec_3,  lty = "dashed")
+lines((1:t_len) / t_len, Y4, type = "l", col = "blue")
+lines((1:t_len) / t_len, lambda_vec_4, col = "blue", lty = "dashed")
+title(main = expression(Plot ~ of ~ the ~ time ~ series ~ 3  ~ and  ~ 4), line = 1)
+
+lambda_vec_5 <- c(rep(1000, t_len/2), rep(4000, t_len/2))
+tmp <- rep(1000, t_len/3)
+lambda_vec_6 <- c(tmp, rep(4000, t_len - length(tmp)))
+
+Y5 <- simulate_data(n_ts = 1, t_len = t_len, lambda_vec = lambda_vec_5, sigma = sigma)
+Y6 <- simulate_data(n_ts = 1, t_len = t_len, lambda_vec = lambda_vec_6, sigma = sigma)
+
+plot((1:t_len) / t_len, Y5,  ylim = c(0, max(Y5, Y6) + 100), xlab="u",
+     ylab = "", mgp=c(2,0.5,0), type = "l")
+lines((1:t_len) / t_len, lambda_vec_5,  lty = "dashed")
+lines((1:t_len) / t_len, Y6, type = "l", col = "blue")
+lines((1:t_len) / t_len, lambda_vec_6, col = "blue", lty = "dashed")
+title(main = expression(Plot ~ of ~ the ~ time ~ series ~ 5  ~ and  ~ 6), line = 1)
+
+Y <- cbind(Y1, Y2, Y3, Y4, Y5, Y6)
 
 b_grid <- seq(1, b_bar, by = 0.01)
-
-
 
 m_hat <- function(vect_u, b, data_p, grid_p, bw){
   m_hat_vec <- c()
@@ -69,19 +95,19 @@ m_hat <- function(vect_u, b, data_p, grid_p, bw){
 }
 
 grid_points <- seq(1/t_len, 1, by = 1/t_len)
-# integral_points <- seq(1/t_len, 1, by = 0.01/t_len)
-# 
-# m_hat_vec <- m_hat(integral_points, b = 1, covid_mat[, 1], grid_points, bw = bw_abs/t_len)
-# plot(grid_points, covid_mat[, 1], type = "l")
-# lines(integral_points, m_hat_vec, col = "red")
-# 
-# m_hat_vec <- m_hat(integral_points, b = 1, covid_mat[, 2], grid_points, bw = bw_abs/t_len)
-# plot(grid_points, covid_mat[, 2], type = "l")
-# lines(integral_points, m_hat_vec, col = "blue")
-# 
-# m_hat_vec <- m_hat(integral_points, b = 1, covid_mat[, 3], grid_points, bw = bw_abs/t_len)
-# plot(grid_points, covid_mat[, 3], type = "l")
-# lines(integral_points, m_hat_vec, col = "green")
+integral_points <- seq(1/t_len, 1, by = 0.01/t_len)
+
+m_hat_vec <- m_hat(integral_points, b = 1, Y[, 1], grid_points, bw = bw_abs/t_len)
+plot(grid_points, Y[, 1], type = "l")
+lines(integral_points, m_hat_vec, col = "red")
+
+m_hat_vec <- m_hat(integral_points, b = 1, Y[, 2], grid_points, bw = bw_abs/t_len)
+plot(grid_points, Y[, 2], type = "l")
+lines(integral_points, m_hat_vec, col = "blue")
+
+m_hat_vec <- m_hat(integral_points, b = 1, Y[, 3], grid_points, bw = bw_abs/t_len)
+plot(grid_points, Y[, 3], type = "l")
+lines(integral_points, m_hat_vec, col = "green")
 
 
 integrand <- function(vect_u, b, data_points_i, data_points_j, 
@@ -97,23 +123,23 @@ for (b in b_grid){
   norm   <- c()
   for (k in 1:n_ts){
     norm_b <- c(norm_b, integrate(m_hat, lower = 0, upper = 1/b, b = b,
-                                  data_p = covid_mat[, k], grid_p = grid_points,
+                                  data_p = Y[, k], grid_p = grid_points,
                                   bw = bw_abs/t_len, subdivisions=2000)$value)
     norm <- c(norm, integrate(m_hat, lower = 0, upper = 1/b, b = 1,
-                              data_p = covid_mat[, k], grid_p = grid_points, 
+                              data_p = Y[, k], grid_p = grid_points, 
                               bw = bw_abs/t_len, subdivisions=2000)$value)
   }
   for (i in 1:(n_ts - 1)){
     for (j in (i + 1):n_ts){
       delta_ij <- 1/b * integrate(integrand, lower = 0, upper = 1/b, b = b,
-                                  data_points_i = covid_mat[, i],
-                                  data_points_j = covid_mat[, j],
+                                  data_points_i = Y[, i],
+                                  data_points_j = Y[, j],
                                   norm_b = norm_b[i], norm = norm[j],
                                   grid_points = grid_points, bw = bw_abs/t_len,
                                   subdivisions=2000)$value
       delta_ji <- 1/b * integrate(integrand, lower = 0, upper = 1/b, b = b,
-                                  data_points_i = covid_mat[, j],
-                                  data_points_j = covid_mat[, i],
+                                  data_points_i = Y[, j],
+                                  data_points_j = Y[, i],
                                   norm_b = norm_b[j], norm = norm[i],
                                   grid_points = grid_points, bw = bw_abs/t_len,
                                   subdivisions=2000)$value
@@ -125,166 +151,13 @@ for (b in b_grid){
         }
       }
       Delta_hat[j, i] <- Delta_hat[i, j]
-      #cat("b = ", b, ", Delta_hat = ", Delta_hat[i, j], "\n")
     }
-  }  
-}
-
-colnames(Delta_hat) <- countries
-rownames(Delta_hat) <- countries
-
-delta_dist <- as.dist(Delta_hat)
-res        <- hclust(delta_dist)
-plot(res)########################
-#Analysis of covid data#
-########################
-rm(list=ls())
-
-library(tidyr)
-library(multiscale)
-library(xtable)
-library(aweek)
-options(xtable.floating = FALSE)
-options(xtable.timestamp = "")
-
-library(dendextend)
-
-source("functions.R")
-
-#Defining necessary constants
-b_bar <- 2
-bw_abs <- 7
-
-#Loading the world coronavirus data
-covid         <- read.csv("data/covid.csv", sep = ",", dec = ".", stringsAsFactors = FALSE, na.strings = "")
-covid         <- covid[!is.na(covid$countryterritoryCode), ]
-covid$dateRep <- as.Date(covid$dateRep, format = "%d/%m/%Y")
-covid         <- complete(covid, dateRep = seq.Date(min(dateRep), max(dateRep), by='day'),
-                          countryterritoryCode, fill = list(cases = 0, deaths = 0))
-
-#Now we "normalize" the data counting only the countries with more than 1000 deaths overall
-#and taking the day of 100th case as the starting point
-covid$weekday         <- weekdays(covid$dateRep)
-covid$cumcases        <- 0
-covid$cumdeaths       <- 0
-
-covid_list <- list()
-for (country in unique(covid$countryterritoryCode)){
-  covid[covid$countryterritoryCode == country, "cumcases"]  <- cumsum(covid[covid$countryterritoryCode == country, "cases"])
-  covid[covid$countryterritoryCode == country, "cumdeaths"] <- cumsum(covid[covid$countryterritoryCode == country, "deaths"])
-  tmp <- max(covid[covid$countryterritoryCode == country, "cumdeaths"])
-  if (tmp >= 1000){
-    tmp_df <- covid[(covid$countryterritoryCode == country & covid$cumcases >= 100),
-                    c("dateRep", "cases", "deaths", "cumcases", "cumdeaths", "weekday")]
-    tmp_index <- match("Monday", tmp_df$weekday)
-    #tmp_index = 1 #If we do not want to normalize by Mondays
-    covid_list[[country]] <- tmp_df[tmp_index:nrow(tmp_df), ]
   }
+  cat("b = ", b, ": done. \n")
 }
 
-
-#Calculate the number of days that we have data for all fivecountries.
-#We are not considering CHN = China as it has too long dataset.
-t_len     <- min(sapply(covid_list[names(covid_list) != "CHN"], NROW))
-#t_len     <- 150 #We consider the first five months of the pandemic
-countries <- names(covid_list)
-dates     <- unique(covid$dateRep)
-n_ts      <- length(covid_list) #Number of time series
-
-#In order not to work with lists, we construct a matrix
-#with number of cases for all countries and.
-#It is not necessary, but it is more convenient to work with.
-covid_mat           <- matrix(NA, ncol = n_ts, nrow = t_len)
-colnames(covid_mat) <- countries
-
-i = 1
-for (country in countries) {
-  covid_mat[, i]        <- covid_list[[country]]$cases[1:t_len]
-  i = i + 1
-}
-
-#Cleaning the data: there are weird cases in the dataset when the number of new cases is negative! 
-sum(covid_mat < 0)
-covid_mat[covid_mat < 0] <- 0
-
-b_grid <- seq(1, b_bar, by = 0.01)
-
-
-
-m_hat <- function(vect_u, b, data_p, grid_p, bw){
-  m_hat_vec <- c()
-  for (u in vect_u){
-    result = sum((abs((grid_p - u * b) / bw) <= 1) * data_p)
-    norm = sum((abs((grid_p - u * b) / bw) <= 1))
-    m_hat_vec <- c(m_hat_vec, result/norm)
-  }
-  return(m_hat_vec)
-}
-
-grid_points <- seq(1/t_len, 1, by = 1/t_len)
-# integral_points <- seq(1/t_len, 1, by = 0.01/t_len)
-# 
-# m_hat_vec <- m_hat(integral_points, b = 1, covid_mat[, 1], grid_points, bw = bw_abs/t_len)
-# plot(grid_points, covid_mat[, 1], type = "l")
-# lines(integral_points, m_hat_vec, col = "red")
-# 
-# m_hat_vec <- m_hat(integral_points, b = 1, covid_mat[, 2], grid_points, bw = bw_abs/t_len)
-# plot(grid_points, covid_mat[, 2], type = "l")
-# lines(integral_points, m_hat_vec, col = "blue")
-# 
-# m_hat_vec <- m_hat(integral_points, b = 1, covid_mat[, 3], grid_points, bw = bw_abs/t_len)
-# plot(grid_points, covid_mat[, 3], type = "l")
-# lines(integral_points, m_hat_vec, col = "green")
-
-
-integrand <- function(vect_u, b, data_points_i, data_points_j, 
-                      norm_b, norm, grid_points, bw) {
-  tmp <- m_hat(vect_u, b, data_points_i, grid_points, bw)/(norm_b * 1/b) - m_hat(vect_u, b = 1, data_points_j, grid_points, bw) / (norm * 1/b)
-  return(tmp^2)
-}
-
-Delta_hat <- matrix(data = rep(0, n_ts * n_ts), nrow = n_ts, ncol = n_ts)
-
-for (b in b_grid){
-  norm_b <- c()
-  norm   <- c()
-  for (k in 1:n_ts){
-    norm_b <- c(norm_b, integrate(m_hat, lower = 0, upper = 1/b, b = b,
-                                  data_p = covid_mat[, k], grid_p = grid_points,
-                                  bw = bw_abs/t_len, subdivisions=2000)$value)
-    norm <- c(norm, integrate(m_hat, lower = 0, upper = 1/b, b = 1,
-                              data_p = covid_mat[, k], grid_p = grid_points, 
-                              bw = bw_abs/t_len, subdivisions=2000)$value)
-  }
-  for (i in 1:(n_ts - 1)){
-    for (j in (i + 1):n_ts){
-      delta_ij <- 1/b * integrate(integrand, lower = 0, upper = 1/b, b = b,
-                                  data_points_i = covid_mat[, i],
-                                  data_points_j = covid_mat[, j],
-                                  norm_b = norm_b[i], norm = norm[j],
-                                  grid_points = grid_points, bw = bw_abs/t_len,
-                                  subdivisions=2000)$value
-      delta_ji <- 1/b * integrate(integrand, lower = 0, upper = 1/b, b = b,
-                                  data_points_i = covid_mat[, j],
-                                  data_points_j = covid_mat[, i],
-                                  norm_b = norm_b[j], norm = norm[i],
-                                  grid_points = grid_points, bw = bw_abs/t_len,
-                                  subdivisions=2000)$value
-      if (b == 1) {
-        Delta_hat[i, j]   <- min(delta_ij, delta_ji)
-      } else {
-        if (min(delta_ij, delta_ji) < Delta_hat[i, j]) {
-          Delta_hat[i, j] <- min(delta_ij, delta_ji)
-        }
-      }
-      Delta_hat[j, i] <- Delta_hat[i, j]
-      #cat("b = ", b, ", Delta_hat = ", Delta_hat[i, j], "\n")
-    }
-  }  
-}
-
-colnames(Delta_hat) <- countries
-rownames(Delta_hat) <- countries
+#colnames(Delta_hat) <- countries
+#rownames(Delta_hat) <- countries
 
 delta_dist <- as.dist(Delta_hat)
 res        <- hclust(delta_dist)
