0f148bd936f76903c67bc5be70de3e967c2464f6
Marina Khismatullina <khismatullina@ese.eur.nl>
2024-02-28 17:03:10 +0100

    Size for Sizer

--- Multiple_trends/Code/sizer.R
+++ Multiple_trends/Code/sizer.R
@@ -25,11 +25,10 @@ n_ts  <- 2 #Number of time series
 n_rep    <- 1000 #number of simulations for calculating size and power
 sim_runs <- 1000 #number of simulations to calculate the Gaussian quantiles
 
-different_T     <- c(100) #Different lengths of time series
+different_T     <- c(100, 250) #Different lengths of time series
 different_alpha <- c(0.01, 0.05, 0.1) #Different confidence levels
 different_b     <- c(0) #Zero is for calculating the size
 
-beta <- NULL
 
 #For the error process
 a     <- 0.25
@@ -50,7 +49,7 @@ size_and_power_array <- array(NA, dim = c(length(different_T),
                               dimnames = list(t = different_T,
                                               b = different_b,
                                               alpha = different_alpha))
-size_and_power_array_sizer <- array(NA, dim = c(length(different_T),
+size_and_power_sizer_array <- array(NA, dim = c(length(different_T),
                                                 length(different_b),
                                                 length(different_alpha)),
                                     dimnames = list(t = different_T,
@@ -62,22 +61,41 @@ ijset <- expand.grid(i = 1:n_ts, j = 1:n_ts)
 ijset <- ijset[ijset$i < ijset$j, ]
 
 for (t_len in different_T){
-  set.seed(seed)
+#  set.seed(seed)
   k <- match(t_len, different_T)
   
-  #Constructing the grid according to the application
+  #Constructing the grid
   u_grid <- seq(from = 5 / t_len, to = 1, by = 5 / t_len)
   h_grid <- seq(from = 2 / t_len, to = 1 / 4, by = 5 / t_len)
   h_grid <- h_grid[h_grid > log(t_len) / t_len]
   grid   <- construct_grid(t = t_len, u_grid = u_grid, h_grid = h_grid)
 
   sigma_vector  <- rep(sqrt(lrv), n_ts)
+  
+  
+  #################################
+  #Calculating the SiZer quantiles#
+  #################################
+  
+  cat("Calculating the SiZer quantiles\n")
+  autocov <- (sigma^2/(1 - a)^2) * (a^seq(0, t_len - 1, by = 1))
+  
+  sizer.quants <- vector("list", length(different_alpha))
+  for (j in 1:length(different_alpha)){
+    sizer.quants[[j]] <- SiZer_quantiles(alpha = different_alpha[j],
+                                         t_len = t_len,
+                                         grid = grid, autocov1 = autocov,
+                                         autocov2 = autocov)
+  }
+  sizer.wghts  <- SiZer_weights(T = t_len, grid = grid)
+  sizer.std    <- SiZer_std(weights = sizer.wghts, autocov1 = autocov,
+                            autocov2 = autocov, t_len = t_len)
+  
   ####################################
-  #Everything for our multiscale test#
+  #Calculating the Gaussian quantiles#
   ####################################
   
-  #Calculating the Gaussian quantiles
-  
+  cat("Calculating the Gaussian quantiles\n")
   simulated_pairwise_gaussian <- matrix(NA, nrow = n_ts * n_ts, ncol = sim_runs)
 
   for (val in 1:sim_runs){
@@ -106,21 +124,25 @@ for (t_len in different_T){
   
   quants <- as.vector(quantiles[2, ])
 
+  #################################
+  #Testing for different scenarios#
+  #################################
+  
   for (b in different_b){
     simulated_pairwise_statistics <- matrix(NA, nrow = n_ts * n_ts, ncol = sim_runs)
-    
+    sizer_results_matrix          <- matrix(NA, nrow = length(different_alpha), ncol = sim_runs)
+        
     m_matrix      <- matrix(0, nrow = t_len, ncol = n_ts)
     if (b == 0) {
       cat("SIZE SIMULATIONS\n")
     } else {
       cat("POWER SIMULATIONS WITH b = ", b, "\n")
-      
       #Only the first trend function is non-zero:
-      #trend function which is a bump
       m_matrix[, 1] <- bump((1:t_len)/t_len) * b
     }
     
     for (val in 1:n_rep){
+      #Simulated data
       y_matrix      <- matrix(NA, nrow = t_len, ncol = n_ts)
       y_augm_matrix <- matrix(NA, nrow = t_len, ncol = n_ts)
       error_matrix  <- matrix(NA, nrow = t_len, ncol = n_ts)
@@ -135,16 +157,40 @@ for (t_len in different_T){
         #Estimating the fixed effects
         alpha_hat_tmp      <- mean(y_matrix[, i])
         y_augm_matrix[, i] <- y_matrix[, i] - alpha_hat_tmp
-      }     
+      }
+      
+      #MULTISCALE TEST
       psi <- compute_statistics(data = y_augm_matrix,
                                 sigma_vec = sigma_vector,
                                 n_ts = n_ts, grid = grid)    
       simulated_pairwise_statistics[, val] <- as.vector(psi$stat_pairwise)
+      
+      #VALUES FOR SIZER TEST
+      values1     <- sizer.wghts %*% y_matrix[, 1]
+      sizer.vals1 <- as.vector(values1)
+      values2     <- sizer.wghts %*% y_matrix[, 2]
+      sizer.vals2 <- as.vector(values2)
+      
+      sizer_results_vec <- c()
+      for (alpha in different_alpha){
+        j <- match(alpha, different_alpha)
+        SiZer_results <- SiZer_test(values1 = sizer.vals1, values2 = sizer.vals2,
+                                    std.devs = sizer.std, quants = sizer.quants[[j]],
+                                    grid = grid)
+        sizer_results_vec <- c(sizer_results_vec, as.integer(sum(abs(SiZer_results$test))>0))
+        
+        #SiZermap(SiZer_results$ugrid, SiZer_results$hgrid, SiZer_results$test, plot.title = NA)
+      }
+      sizer_results_matrix[, val] <- as.vector(sizer_results_vec)
     }
+    
     simulated_statistic <- apply(simulated_pairwise_statistics[1:(n_ts * n_ts), ], 2, max)
     
-    size_and_power_vec <- c()
+    size_and_power_vec       <- c()
+    size_and_power_sizer_vec <- c()
+    
     for (alpha in different_alpha){
+      j <- match(alpha, different_alpha)
       if (sum(probs == (1 - alpha)) == 0)
         pos <- which.min(abs(probs - (1 - alpha)))
       if (sum(probs == (1 - alpha)) != 0)
@@ -156,25 +202,18 @@ for (t_len in different_T){
       
       cat("Ratio of rejection is ", num_of_rej, "with b = ", b,
           ", alpha = ", alpha, "and T = ", t_len, "\n")
+      
+      num_of_rej_sizer         <- sum(sizer_results_matrix[, j])/n_rep
+      size_and_power_sizer_vec <- c(size_and_power_sizer_vec, num_of_rej_sizer) 
+      
+      cat("Ratio of rejection for SiZer is ", num_of_rej_sizer, "with b = ", b,
+          ", alpha = ", alpha, "and T = ", t_len, "\n")
     }
     
     #Storing the results in a 3D array
     l <- match(b, different_b)
-    size_and_power_array[k, l, ] <- size_and_power_vec
-  }
-  
-  ###############################
-  #Everything for the Sizer test#
-  ###############################  
-  #Calculating the SiZer quantiles
-  autocov <- (sigma^2/(1 - a)^2) * (a^seq(0, t_len - 1, by = 1))
-  
-  sizer.quants <- vector("list", length(different_alpha))
-  for (j in 1:length(different_alpha)){
-    sizer.quants[[j]] <- SiZer_quantiles(alpha = different_alpha[j],
-                                         t_len = t_len,
-                                         grid = grid, autocov1 = autocov,
-                                         autocov2 = autocov) 
+    size_and_power_array[k, l, ]       <- size_and_power_vec
+    size_and_power_sizer_array[k, l, ] <- size_and_power_sizer_vec
   }
 } 
   
